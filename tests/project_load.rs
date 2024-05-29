11ba89f93eebcb62cacb21994aebe200a7c0567e
ScriptAndCompile <scriptandcompile@gmail.com>
2024-05-29 16:00:25 -0700

    Switched to winnow from nom. Using BStr instead of 'String-ifying' for near 'no-copy' parsing.

--- tests/project_load.rs
+++ tests/project_load.rs
@@ -1,4 +1,5 @@
-use vb6parse::parser::VB6Project;
+use bstr::{ByteSlice, B};
+use vb6parse::parser::{ProjectType, VB6Project};
 
 #[test]
 fn vbp_load() {
@@ -6,6 +7,7 @@ fn vbp_load() {
 
     let project = VB6Project::parse(project_file_bytes).unwrap();
 
+    assert_eq!(project.project_type, ProjectType::Exe);
     assert_eq!(project.references.len(), 15);
     assert_eq!(project.objects.len(), 12);
     assert_eq!(project.modules.len(), 39);
@@ -17,32 +19,67 @@ fn vbp_load() {
 
     assert_eq!(
         project.res_file_32_path,
-        Some("..\\DBCommon\\PSFC.RES".to_owned())
+        Some(b"..\\DBCommon\\PSFC.RES".as_bstr())
     );
-    assert_eq!(project.icon_form, Some("frmMain".to_owned()));
-    assert_eq!(project.startup, Some("Sub Main".to_owned()));
-    assert_eq!(project.help_file_path, Some("".to_owned()));
-    assert_eq!(project.title, Some("PPDM".to_owned()));
-    assert_eq!(project.exe_32_file_name, Some("PPDM.exe".to_owned()));
+    assert_eq!(project.icon_form, Some(b"frmMain".as_bstr()));
+    assert_eq!(project.startup, Some(b"Sub Main".as_bstr()));
+    assert_eq!(project.help_file_path, Some(b"".as_bstr()));
+    assert_eq!(project.title, Some(b"PPDM".as_bstr()));
+    assert_eq!(project.exe_32_file_name, Some(b"PPDM.exe".as_bstr()));
     assert_eq!(
         project.command_line_arguments,
-        Some("-DisableRememberPassword%20-CHARTING -U -language %22english%7d".to_owned())
+        Some(b"-DisableRememberPassword%20-CHARTING -U -language %22english%7d".as_bstr())
     );
-    assert_eq!(project.name, Some("PPDM".to_owned()));
-    assert_eq!(project.help_context_id, Some("0".to_owned()));
-    assert_eq!(project.compatible_mode, Some("0".to_owned()));
+    assert_eq!(project.name, Some(b"PPDM".as_bstr()));
+    assert_eq!(project.help_context_id, Some(b"0".as_bstr()));
+    assert_eq!(project.compatible_mode, false);
     assert_eq!(project.upgrade_activex_controls, true);
-    assert_eq!(project.major_version, Some("11".to_owned()));
-    assert_eq!(project.minor_version, Some("0".to_owned()));
-    assert_eq!(project.revision_version, Some("288".to_owned()));
-    assert_eq!(project.auto_increment_revision_version, false);
     assert_eq!(project.server_support_files, false);
     assert_eq!(
-        project.version_company_name,
-        Some("Predator Software Inc.".to_owned())
+        project.conditional_compile,
+        Some(b"PDMBuild = 1 : PDM_SHORTCUTS = 1 : PMData7Build = 0".as_bstr())
+    );
+    assert_eq!(project.auto_refresh, true);
+    assert_eq!(project.compilation_type, false);
+    assert_eq!(project.optimization_type, false);
+    assert_eq!(project.favor_pentium_pro, false);
+    assert_eq!(project.code_view_debug_info, false);
+    assert_eq!(project.aliasing, true);
+    assert_eq!(project.bounds_check, false);
+    assert_eq!(project.overflow_check, false);
+    assert_eq!(project.floating_point_check, false);
+    assert_eq!(project.pentium_fdiv_bug_check, false);
+    assert_eq!(project.unrounded_floating_point, false);
+    assert_eq!(project.start_mode, false);
+    assert_eq!(project.unattended, false);
+    assert_eq!(project.retained, false);
+    assert_eq!(project.thread_per_object, 0);
+    assert_eq!(project.max_number_of_threads, 1);
+    assert_eq!(project.debug_startup_option, false);
+
+    // version information.
+    assert_eq!(project.version_info.major, 11);
+    assert_eq!(project.version_info.minor, 0);
+    assert_eq!(project.version_info.revision, 288);
+    assert_eq!(project.version_info.auto_increment_revision, false);
+    assert_eq!(
+        project.version_info.company_name,
+        Some(b"Predator Software Inc.".as_bstr())
     );
     assert_eq!(
-        project.version_file_description,
-        Some("Predator PDM ".to_owned())
+        project.version_info.file_description,
+        Some(b"Predator PDM ".as_bstr())
+    );
+    assert_eq!(
+        project.version_info.copyright,
+        Some(B("Copyright �1994 - 2022 Predator Software Inc.  All Rights Reserved.").as_bstr())
+    );
+    assert_eq!(
+        project.version_info.trademark,
+        Some(B("Predator SFC� and Predator PDM� are Registered Trademarks of Predator Software Inc.").as_bstr())
+    );
+    assert_eq!(
+        project.version_info.product_name,
+        Some(B("Predator PDM").as_bstr())
     );
 }
