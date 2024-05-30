5cf783ec48e543a0b9694965866d1bfb1aca91c4
ScriptAndCompile <scriptandcompile@gmail.com>
2024-05-30 11:58:25 -0700

    Added Map Editor integration test and benchmark.

--- tests/vb6_code.rs
+++ tests/vb6_code.rs
@@ -985,7 +985,6 @@ fn gradient_2d_project_load() {
     assert_eq!(project.version_info.product_name, Some(B("").as_bstr()));
 }
 
-
 #[test]
 fn grayscale_effect_project_load() {
     let project_file_bytes = include_bytes!("./data/vb6-code/Grayscale-effect/Grayscale.vbp");
@@ -1043,7 +1042,10 @@ fn grayscale_effect_project_load() {
         Some(b"Tanner Helland Productions".as_bstr())
     );
     assert_eq!(project.version_info.file_description, Some(b"".as_bstr()));
-    assert_eq!(project.version_info.copyright, Some(b"Published in 2011 by Tanner Helland".as_bstr()));
+    assert_eq!(
+        project.version_info.copyright,
+        Some(b"Published in 2011 by Tanner Helland".as_bstr())
+    );
     assert_eq!(project.version_info.trademark, Some(B("").as_bstr()));
     assert_eq!(project.version_info.product_name, Some(B("").as_bstr()));
 }
@@ -1112,7 +1114,8 @@ fn hidden_markov_model_project_load() {
 
 #[test]
 fn histograms_advanced_project_load() {
-    let project_file_bytes = include_bytes!("./data/vb6-code/Histograms-advanced/Advanced Histograms.vbp");
+    let project_file_bytes =
+        include_bytes!("./data/vb6-code/Histograms-advanced/Advanced Histograms.vbp");
 
     let project = VB6Project::parse(project_file_bytes).unwrap();
 
@@ -1131,7 +1134,10 @@ fn histograms_advanced_project_load() {
     assert_eq!(project.startup, Some(b"frmMain".as_bstr()));
     assert_eq!(project.help_file_path, Some(b"".as_bstr()));
     assert_eq!(project.title, Some(b"Advanced Histograms".as_bstr()));
-    assert_eq!(project.exe_32_file_name, Some(b"Advanced Histogram Viewer.exe".as_bstr()));
+    assert_eq!(
+        project.exe_32_file_name,
+        Some(b"Advanced Histogram Viewer.exe".as_bstr())
+    );
     assert_eq!(project.command_line_arguments, Some(b"".as_bstr()));
     assert_eq!(project.name, Some(b"Histogram_Demo".as_bstr()));
     assert_eq!(project.help_context_id, Some(b"0".as_bstr()));
@@ -1169,12 +1175,16 @@ fn histograms_advanced_project_load() {
     assert_eq!(project.version_info.file_description, Some(b"".as_bstr()));
     assert_eq!(project.version_info.copyright, Some(b"".as_bstr()));
     assert_eq!(project.version_info.trademark, Some(B("").as_bstr()));
-    assert_eq!(project.version_info.product_name, Some(B("Visual Basic Image Histogram Example").as_bstr()));
+    assert_eq!(
+        project.version_info.product_name,
+        Some(B("Visual Basic Image Histogram Example").as_bstr())
+    );
 }
 
 #[test]
 fn histogram_basic_project_load() {
-    let project_file_bytes = include_bytes!("./data/vb6-code/Histograms-basic/Basic Histograms.vbp");
+    let project_file_bytes =
+        include_bytes!("./data/vb6-code/Histograms-basic/Basic Histograms.vbp");
 
     let project = VB6Project::parse(project_file_bytes).unwrap();
 
@@ -1193,7 +1203,10 @@ fn histogram_basic_project_load() {
     assert_eq!(project.startup, Some(b"frmMain".as_bstr()));
     assert_eq!(project.help_file_path, Some(b"".as_bstr()));
     assert_eq!(project.title, Some(b"Advanced Histograms".as_bstr()));
-    assert_eq!(project.exe_32_file_name, Some(b"Basic Histogram Viewer.exe".as_bstr()));
+    assert_eq!(
+        project.exe_32_file_name,
+        Some(b"Basic Histogram Viewer.exe".as_bstr())
+    );
     assert_eq!(project.command_line_arguments, Some(b"".as_bstr()));
     assert_eq!(project.name, Some(b"Histogram_Demo".as_bstr()));
     assert_eq!(project.help_context_id, Some(b"0".as_bstr()));
@@ -1231,7 +1244,10 @@ fn histogram_basic_project_load() {
     assert_eq!(project.version_info.file_description, Some(b"".as_bstr()));
     assert_eq!(project.version_info.copyright, Some(b"".as_bstr()));
     assert_eq!(project.version_info.trademark, Some(B("").as_bstr()));
-    assert_eq!(project.version_info.product_name, Some(B("Visual Basic Image Histogram Example").as_bstr()));
+    assert_eq!(
+        project.version_info.product_name,
+        Some(B("Visual Basic Image Histogram Example").as_bstr())
+    );
 }
 
 #[test]
@@ -1255,7 +1271,10 @@ fn levels_effect_project_load() {
     assert_eq!(project.startup, Some(b"frmMain".as_bstr()));
     assert_eq!(project.help_file_path, Some(b"".as_bstr()));
     assert_eq!(project.title, Some(b"Image Levels".as_bstr()));
-    assert_eq!(project.exe_32_file_name, Some(b"Image Levels.exe".as_bstr()));
+    assert_eq!(
+        project.exe_32_file_name,
+        Some(b"Image Levels.exe".as_bstr())
+    );
     assert_eq!(project.command_line_arguments, Some(b"".as_bstr()));
     assert_eq!(project.name, Some(b"Image_Levels_Project".as_bstr()));
     assert_eq!(project.help_context_id, Some(b"0".as_bstr()));
@@ -1293,9 +1312,11 @@ fn levels_effect_project_load() {
     assert_eq!(project.version_info.file_description, Some(b"".as_bstr()));
     assert_eq!(project.version_info.copyright, Some(b"".as_bstr()));
     assert_eq!(project.version_info.trademark, Some(B("").as_bstr()));
-    assert_eq!(project.version_info.product_name, Some(B("Visual Basic Image Levels Example").as_bstr()));
+    assert_eq!(
+        project.version_info.product_name,
+        Some(B("Visual Basic Image Levels Example").as_bstr())
+    );
 }
-
 
 #[test]
 fn mandelbrot_project_load() {
@@ -1354,7 +1375,75 @@ fn mandelbrot_project_load() {
         Some(b"Tanner Helland Productions".as_bstr())
     );
     assert_eq!(project.version_info.file_description, Some(b"".as_bstr()));
-    assert_eq!(project.version_info.copyright, Some(b"Released under a BSD License; see www.tannerhelland.com for details.".as_bstr()));
+    assert_eq!(
+        project.version_info.copyright,
+        Some(b"Released under a BSD License; see www.tannerhelland.com for details.".as_bstr())
+    );
     assert_eq!(project.version_info.trademark, Some(B("").as_bstr()));
     assert_eq!(project.version_info.product_name, Some(B("").as_bstr()));
+}
+
+#[test]
+fn map_editor_2d_project_load() {
+    let project_file_bytes = include_bytes!("./data/vb6-code/Map-editor-2D/Map Editor.vbp");
+
+    let project = VB6Project::parse(project_file_bytes).unwrap();
+
+    assert_eq!(project.project_type, ProjectType::Exe);
+    assert_eq!(project.references.len(), 1);
+    assert_eq!(project.objects.len(), 0);
+    assert_eq!(project.modules.len(), 2);
+    assert_eq!(project.classes.len(), 1);
+    assert_eq!(project.designers.len(), 0);
+    assert_eq!(project.forms.len(), 2);
+    assert_eq!(project.user_controls.len(), 0);
+    assert_eq!(project.user_documents.len(), 0);
+
+    assert_eq!(project.res_file_32_path, Some(b"".as_bstr()));
+    assert_eq!(project.icon_form, Some(b"Main".as_bstr()));
+    assert_eq!(project.startup, Some(b"Main".as_bstr()));
+    assert_eq!(project.help_file_path, Some(b"".as_bstr()));
+    assert_eq!(project.title, Some(b"Map Editor".as_bstr()));
+    assert_eq!(project.exe_32_file_name, Some(b"Map Editor.exe".as_bstr()));
+    assert_eq!(project.command_line_arguments, Some(b"".as_bstr()));
+    assert_eq!(project.name, Some(b"VB_Map_Editor".as_bstr()));
+    assert_eq!(project.help_context_id, Some(b"0".as_bstr()));
+    assert_eq!(project.compatible_mode, false);
+    assert_eq!(project.upgrade_activex_controls, true);
+    assert_eq!(project.server_support_files, false);
+    assert_eq!(project.conditional_compile, Some(b"".as_bstr()));
+    assert_eq!(project.auto_refresh, true);
+    assert_eq!(project.compilation_type, false);
+    assert_eq!(project.optimization_type, false);
+    assert_eq!(project.favor_pentium_pro, true);
+    assert_eq!(project.code_view_debug_info, false);
+    assert_eq!(project.aliasing, false);
+    assert_eq!(project.bounds_check, true);
+    assert_eq!(project.overflow_check, true);
+    assert_eq!(project.floating_point_check, true);
+    assert_eq!(project.pentium_fdiv_bug_check, true);
+    assert_eq!(project.unrounded_floating_point, true);
+    assert_eq!(project.start_mode, false);
+    assert_eq!(project.unattended, false);
+    assert_eq!(project.retained, false);
+    assert_eq!(project.thread_per_object, 0);
+    assert_eq!(project.max_number_of_threads, 1);
+    assert_eq!(project.debug_startup_option, false);
+
+    // version information.
+    assert_eq!(project.version_info.major, 1);
+    assert_eq!(project.version_info.minor, 0);
+    assert_eq!(project.version_info.revision, 14);
+    assert_eq!(project.version_info.auto_increment_revision, 1);
+    assert_eq!(
+        project.version_info.company_name,
+        Some(b"Tanner Helland Productions".as_bstr())
+    );
+    assert_eq!(project.version_info.file_description, Some(b"".as_bstr()));
+    assert_eq!(
+        project.version_info.copyright,
+        Some(b"".as_bstr())
+    );
+    assert_eq!(project.version_info.trademark, Some(B("").as_bstr()));
+    assert_eq!(project.version_info.product_name, Some(B("VB Map Editor").as_bstr()));
 }
