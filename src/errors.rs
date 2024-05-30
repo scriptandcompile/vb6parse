2b1a210de55a9e783c24c4fd695d8afc2d0a9b09
ScriptAndCompile <scriptandcompile@gmail.com>
2024-05-29 20:32:03 -0700

    updated errors to have all of the 'unparsable' error types.

--- src/errors.rs
+++ src/errors.rs
@@ -32,10 +32,45 @@ pub enum VB6ProjectParseError<I> {
     MinorVersionUnparseable,
     #[error("Revision version is not a number.")]
     RevisionVersionUnparseable,
-    #[error("AutoIncrement can only be a 0 (false) or a 1 (true)")]
+    #[error("AutoIncrement can only be a 0 (false) or a -1 (true)")]
     AutoIncrementUnparseable,
-    #[error("CompatibilityMode can only be a 0 (false) or a 1 (true)")]
+    #[error("CompatibilityMode can only be a 0 (false) or a -1 (true)")]
     CompatibilityModeUnparseable,
+    #[error("NoControlUpgrade can only be a 0 (false) or a -1 (true)")]
+    NoControlUpgradeUnparsable,
+    #[error("ServerSupportFiles can only be a 0 (false) or a -1 (true)")]
+    ServerSupportFilesUnparseable,
+    #[error("CompilationType can only be a 0 (false) or a -1 (true)")]
+    CompilationTypeUnparseable,
+    #[error("OptimizationType can only be a 0 (false) or a -1 (true)")]
+    OptimizationTypeUnparseable,
+    #[error("FavorPentiumPro(tm) can only be a 0 (false) or a -1 (true)")]
+    FavorPentiumProUnparseable,
+    #[error("CodeViewDebugInfo can only be a 0 (false) or a -1 (true)")]
+    CodeViewDebugInfoUnparseable,
+    #[error("NoAliasing can only be a 0 (false) or a -1 (true)")]
+    NoAliasingUnparseable,
+    #[error("oundsCheck can only be a 0 (false) or a -1 (true)")]
+    BoundsCheckUnparseable,
+    #[error("OverflowCheck can only be a 0 (false) or a -1 (true)")]
+    OverflowCheckUnparseable,
+    #[error("FlPointCheck can only be a 0 (false) or a -1 (true)")]
+    FlPointCheckUnparseable,
+    #[error("FDIVCheck can only be a 0 (false) or a -1 (true)")]
+    FDIVCheckUnparseable,
+    #[error("UnroundedFP can only be a 0 (false) or a -1 (true)")]
+    UnroundedFPUnparseable,
+    #[error("StartMode can only be a 0 (false) or a -1 (true)")]
+    StartModeUnparseable,
+    #[error("Unattended can only be a 0 (false) or a -1 (true)")]
+    UnattendedUnparseable,
+    #[error("Retained can only be a 0 (false) or a -1 (true)")]
+    RetainedUnparseable,
+    #[error("DebugStartup can only be a 0 (false) or a -1 (true)")]
+    DebugStartupOptionUnparseable,
+    #[error("AutoRefresh can only be a 0 (false) or a -1 (true)")]
+    AutoRefreshUnparseable,
+
     #[error("Thread Per Object is not a number.")]
     ThreadPerObjectUnparseable,
     #[error("Max Threads is not a number.")]
