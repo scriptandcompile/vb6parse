2659ff2aa8b8e2ca4615e538dbb290939319a633
ScriptAndCompile <scriptandcompile@gmail.com>
2024-05-29 22:34:27 -0700

    Added a whole host of integration test data projects and test integration loaders.

--- tests/vb6_code/brightness_effect.rs
+++ tests/vb6_code/brightness_effect.rs
@@ -2,38 +2,39 @@ use bstr::{ByteSlice, B};
 use vb6parse::project::{ProjectType, VB6Project};
 
 #[test]
-fn vb6_code_artificial_life_project_load() {
-    let project_file_bytes = include_bytes!("./vb6-code/Artificial-life/Artificial Life.vbp");
+fn artificial_life_project_load() {
+    let project_file_bytes =
+        include_bytes!("../data/vb6-code/Brightness-effect/Part 1 - Pure VB6/Brightness.vbp");
 
     let project = VB6Project::parse(project_file_bytes).unwrap();
 
     assert_eq!(project.project_type, ProjectType::Exe);
     assert_eq!(project.references.len(), 1);
     assert_eq!(project.objects.len(), 0);
-    assert_eq!(project.modules.len(), 1);
-    assert_eq!(project.classes.len(), 2);
+    assert_eq!(project.modules.len(), 0);
+    assert_eq!(project.classes.len(), 0);
     assert_eq!(project.designers.len(), 0);
     assert_eq!(project.forms.len(), 1);
     assert_eq!(project.user_controls.len(), 0);
     assert_eq!(project.user_documents.len(), 0);
 
     assert_eq!(project.res_file_32_path, Some(b"".as_bstr()));
-    assert_eq!(project.icon_form, Some(b"frmMain".as_bstr()));
-    assert_eq!(project.startup, Some(b"frmMain".as_bstr()));
+    assert_eq!(project.icon_form, Some(b"frmBrightness".as_bstr()));
+    assert_eq!(project.startup, Some(b"frmBrightness".as_bstr()));
     assert_eq!(project.help_file_path, Some(b"".as_bstr()));
-    assert_eq!(project.title, Some(b"Artificial Life Simulator".as_bstr()));
+    assert_eq!(project.title, Some(b"vbBrightness".as_bstr()));
     assert_eq!(
         project.exe_32_file_name,
-        Some(b"Artificial Life.exe".as_bstr())
+        Some(b"vbBrightness.exe".as_bstr())
     );
     assert_eq!(project.command_line_arguments, Some(b"".as_bstr()));
-    assert_eq!(project.name, Some(b"Artificial_Life".as_bstr()));
+    assert_eq!(project.name, Some(b"VB_Brightness".as_bstr()));
     assert_eq!(project.help_context_id, Some(b"0".as_bstr()));
     assert_eq!(project.compatible_mode, false);
     assert_eq!(project.upgrade_activex_controls, true);
     assert_eq!(project.server_support_files, false);
     assert_eq!(project.conditional_compile, Some(b"".as_bstr()));
-    assert_eq!(project.auto_refresh, true);
+    assert_eq!(project.auto_refresh, false);
     assert_eq!(project.compilation_type, false);
     assert_eq!(project.optimization_type, false);
     assert_eq!(project.favor_pentium_pro, true);
@@ -53,27 +54,21 @@ fn vb6_code_artificial_life_project_load() {
 
     // version information.
     assert_eq!(project.version_info.major, 1);
-    assert_eq!(project.version_info.minor, 2);
-    assert_eq!(project.version_info.revision, 76);
+    assert_eq!(project.version_info.minor, 0);
+    assert_eq!(project.version_info.revision, 18);
     assert_eq!(project.version_info.auto_increment_revision, 1);
-    assert_eq!(
-        project.version_info.company_name,
-        Some(b"Tanner Helland Productions".as_bstr())
-    );
+    assert_eq!(project.version_info.company_name, Some(b"".as_bstr()));
     assert_eq!(
         project.version_info.file_description,
-        Some(b"".as_bstr())
+        Some(b"Sample executable".as_bstr())
     );
     assert_eq!(
         project.version_info.copyright,
-        Some(b"\xA92009 Tanner Helland - www.tannerhelland.com".as_bstr())
+        Some(b"\xA92020 Tanner Helland - www.tannerhelland.com".as_bstr())
     );
-    assert_eq!(
-        project.version_info.trademark,
-        Some(B("").as_bstr())
-    );
+    assert_eq!(project.version_info.trademark, Some(B("").as_bstr()));
     assert_eq!(
         project.version_info.product_name,
-        Some(B("Artificial Life Simulator").as_bstr())
+        Some(B("vbBrightness.exe").as_bstr())
     );
 }
