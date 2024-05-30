4f38f93e7ed63df5b1871a4771dc70ba043a5296
ScriptAndCompile <scriptandcompile@gmail.com>
2024-05-30 01:32:19 -0700

    Added first-pass Criterion benchmarks.

---
+++ benches/project_load.rs
@@ -0,0 +1,32 @@
+use vb6parse::project::VB6Project;
+
+use criterion::{black_box, criterion_group, criterion_main, Criterion};
+
+fn criterion_benchmark(c: &mut Criterion) {
+    let projects = vec![
+        include_bytes!("../tests/data/vb6-code/Artificial-life/Artificial Life.vbp").to_vec(),
+        include_bytes!("../tests/data/vb6-code/Blacklight-effect/Blacklight.vbp").to_vec(),
+        include_bytes!("../tests/data/vb6-code/Brightness-effect/Part 1 - Pure VB6/Brightness.vbp")
+            .to_vec(),
+        include_bytes!("../tests/data/vb6-code/Color-shift-effect/ShiftColor.vbp").to_vec(),
+        include_bytes!("../tests/data/vb6-code/Colorize-effect/Colorize.vbp").to_vec(),
+        include_bytes!("../tests/data/vb6-code/Contrast-effect/Contrast.vbp").to_vec(),
+        include_bytes!("../tests/data/vb6-code/Curves-effect/Curves.vbp").to_vec(),
+        include_bytes!("../tests/data/vb6-code/Custom-image-filters/CustomFilters.vbp").to_vec(),
+        include_bytes!("../tests/data/vb6-code/Diffuse-effect/Diffuse.vbp").to_vec(),
+        include_bytes!("../tests/data/vb6-code/Edge-detection/EdgeDetection.vbp").to_vec(),
+    ];
+
+    c.bench_function("load multiple projects", |b| {
+        b.iter(|| {
+            for project in &projects {
+                black_box({
+                    let _proj = VB6Project::parse(project.as_slice());
+                });
+            }
+        })
+    });
+}
+
+criterion_group!(benches, criterion_benchmark);
+criterion_main!(benches);
