3b9a5ee2f4388443d63d1a5959ceff0208a0ab78
ScriptAndCompile <scriptandcompile@gmail.com>
2024-05-30 13:05:12 -0700

    Added Scanner-TWAIN Integration test and benchmark.

--- benches/project_load.rs
+++ benches/project_load.rs
@@ -30,7 +30,7 @@ fn criterion_benchmark(c: &mut Criterion) {
         include_bytes!("../tests/data/vb6-code/Map-editor-2D/Map Editor.vbp").to_vec(),
         include_bytes!("../tests/data/vb6-code/Nature-effects/NatureFilters.vbp").to_vec(),
         include_bytes!("../tests/data/vb6-code/Randomize-effects/RandomizationFX.vbp").to_vec(),
-
+        include_bytes!("../tests/data/vb6-code/Scanner-TWAIN/VB_Scanner_Support.vbp").to_vec(),
     ];
 
     c.bench_function("load multiple projects", |b| {
