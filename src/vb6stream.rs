bf8f907eb934d3d6f25c203137876e144a2cdb46
ScriptAndCompile <scriptandcompile@gmail.com>
2024-07-24 13:39:58 -0700

    Fixed some bugs in the vb6stream. it should match the stream behaviour of a byte slice now.

--- src/vb6stream.rs
+++ src/vb6stream.rs
@@ -6,6 +6,8 @@ use winnow::stream::{Compare, CompareResult, FindSlice, Offset, Stream, StreamIs
 use core::fmt::Debug;
 use core::iter::{Cloned, Enumerate, Iterator};
 use core::slice::Iter;
+
+use core::num::NonZeroUsize;
 use std::num::NonZero;
 
 #[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord, Hash, Default)]
@@ -23,19 +25,19 @@ impl<'a> VB6Stream<'a> {
     }
 
     pub fn is_empty(&self) -> bool {
-        self.stream.is_empty()
+        self.stream.len() == self.index
     }
 }
 
 impl<'a> Offset for VB6Stream<'a> {
     fn offset_from(&self, start: &Self) -> usize {
-        start.stream.len() - self.stream.len()
+        start.stream.len() - self.index
     }
 }
 
 impl<'a> FindSlice<&str> for VB6Stream<'a> {
     fn find_slice(&self, needle: &str) -> Option<std::ops::Range<usize>> {
-        self.stream
+        self.stream[self.index..]
             .find(needle)
             .map(|start| start..start + needle.len())
     }
@@ -44,8 +46,7 @@ impl<'a> FindSlice<&str> for VB6Stream<'a> {
 impl<'a> FindSlice<(&str, &str)> for VB6Stream<'a> {
     fn find_slice(&self, needle: (&str, &str)) -> Option<std::ops::Range<usize>> {
         for needle in &[needle.0, needle.1] {
-            if let Some(range) = self
-                .stream
+            if let Some(range) = self.stream[self.index..]
                 .find(needle)
                 .map(|start| start..start + needle.len())
             {
@@ -59,9 +60,9 @@ impl<'a> FindSlice<(&str, &str)> for VB6Stream<'a> {
 
 impl<'a> Compare<char> for VB6Stream<'a> {
     fn compare(&self, other: char) -> CompareResult {
-        if self.stream.len() < 1 {
+        if self.stream[self.index..].len() < 1 {
             CompareResult::Incomplete
-        } else if self.stream.starts_with(other.to_string().as_bytes()) {
+        } else if self.stream[self.index..].starts_with(other.to_string().as_bytes()) {
             CompareResult::Ok(0)
         } else {
             CompareResult::Error
@@ -74,9 +75,9 @@ impl<'a> Compare<&str> for VB6Stream<'a> {
         let other = other.as_bytes();
         let len = other.len();
 
-        if self.stream.len() < len {
+        if self.stream[self.index..].len() < len {
             CompareResult::Incomplete
-        } else if self.stream.starts_with(other) {
+        } else if self.stream[self.index..].starts_with(other) {
             CompareResult::Ok(len)
         } else {
             CompareResult::Error
@@ -89,9 +90,9 @@ impl<'a> Compare<Caseless<&str>> for VB6Stream<'a> {
         let other = other.as_bytes();
         let len = other.0.len();
 
-        if self.stream.len() < len {
+        if self.stream[self.index..].len() < len {
             CompareResult::Incomplete
-        } else if self.stream[0..len].eq_ignore_ascii_case(other.0) {
+        } else if self.stream[self.index..].eq_ignore_ascii_case(other.0) {
             CompareResult::Ok(len)
         } else {
             CompareResult::Error
@@ -103,9 +104,7 @@ impl<'a> StreamIsPartial for VB6Stream<'a> {
     type PartialState = usize;
 
     fn complete(&mut self) -> usize {
-        self.index = self.stream.len();
-
-        self.index
+        self.stream[self.index..].len() - self.index
     }
 
     fn is_partial(&self) -> bool {
@@ -117,7 +116,7 @@ impl<'a> StreamIsPartial for VB6Stream<'a> {
     }
 
     fn is_partial_supported() -> bool {
-        true
+        false
     }
 }
 
@@ -136,10 +135,8 @@ impl<'a> Stream for VB6Stream<'a> {
     }
 
     fn next_token(&mut self) -> Option<Self::Token> {
-        let (token, next) = self.stream.split_first()?;
-        let index = self.index;
-        *self = VB6Stream::new(next);
-        self.index = index + 1;
+        let (token, _) = self.stream[self.index..].split_first()?;
+        self.index = self.index + 1;
         Some(*token)
     }
 
@@ -147,11 +144,14 @@ impl<'a> Stream for VB6Stream<'a> {
     where
         P: Fn(Self::Token) -> bool,
     {
-        self.stream.iter().position(|b| predicate(*b))
+        self.stream[self.index..].iter().position(|b| predicate(*b))
     }
 
     fn offset_at(&self, tokens: usize) -> Result<usize, Needed> {
-        if let Some(needed) = tokens.checked_sub(self.stream.len()).and_then(NonZero::new) {
+        if let Some(needed) = tokens
+            .checked_sub(self.stream[self.index..].len())
+            .and_then(NonZeroUsize::new)
+        {
             Err(Needed::Size(needed))
         } else {
             Ok(tokens)
@@ -159,8 +159,8 @@ impl<'a> Stream for VB6Stream<'a> {
     }
 
     fn next_slice(&mut self, offset: usize) -> Self::Slice {
-        let (slice, rest) = self.stream.split_at(offset);
-        self.stream = bstr::BStr::new(rest);
+        let (slice, _) = self.stream[self.index..].split_at(offset);
+        self.index = self.index + offset;
         bstr::BStr::new(slice)
     }
 
@@ -169,7 +169,7 @@ impl<'a> Stream for VB6Stream<'a> {
     }
 
     fn reset(&mut self, checkpoint: &Self::Checkpoint) {
-        self.stream = checkpoint.stream;
+        self.index = checkpoint.index;
     }
 
     fn raw(&self) -> &dyn Debug {
@@ -181,94 +181,225 @@ impl<'a> Stream for VB6Stream<'a> {
 mod tests {
     use super::*;
 
+    // Because the documentation of how stream is supposed to operate is a bit
+    // unclear, these unit tests are based on the behavior of the winnow crate
+    // and confirm that our behavior is consistent with their behavior.
+    //
+    // We first test with a normal stream, then with a winnow string stream.
+    //
+    // The two streams should behave the same way.
     #[test]
-    fn test_vb6_stream_offset_for() {
+    fn next_slice() {
+        let mut wstream = b"Hello, World!".as_slice();
+        let mut stream = VB6Stream::new(b"Hello, World!");
+
+        assert_eq!(wstream.next_slice(5), "Hello".as_bytes().as_bstr());
+        assert_eq!(stream.next_slice(5), "Hello".as_bytes().as_bstr());
+
+        assert_eq!(wstream.next_slice(2), ", ".as_bytes().as_bstr());
+        assert_eq!(stream.next_slice(2), ", ".as_bytes().as_bstr());
+
+        assert_eq!(wstream.next_slice(6), "World!".as_bytes().as_bstr());
+        assert_eq!(stream.next_slice(6), "World!".as_bytes().as_bstr());
+    }
+
+    #[test]
+    fn offset_at() {
+        let wstream = b"Hello, World!".as_slice();
+        let stream = VB6Stream::new(b"Hello, World!");
+
+        // Test offset_at with a valid offset
+        assert_eq!(wstream.offset_at(5), Ok(5));
+        assert_eq!(stream.offset_at(5), Ok(5));
+
+        // Test offset_at with an offset that is on the last element
+        assert_eq!(wstream.offset_at(13), Ok(13));
+        assert_eq!(stream.offset_at(13), Ok(13));
+
+        // Test offset_at with an offset that is too large
+        assert_eq!(
+            wstream.offset_at(14),
+            Err(winnow::error::Needed::Size(NonZeroUsize::new(1).unwrap()))
+        );
+        assert_eq!(
+            stream.offset_at(14),
+            Err(winnow::error::Needed::Size(NonZeroUsize::new(1).unwrap()))
+        );
+    }
+
+    #[test]
+    fn offset_for() {
+        let wstream = b"Hello, World!".as_slice();
         let stream = VB6Stream::new(b"Hello, World!");
 
         // Test offset_for with a predicate that matches 'e'
+        assert_eq!(wstream.offset_for(|b| b == b'e'), Some(1));
         assert_eq!(stream.offset_for(|b| b == b'e'), Some(1));
 
         // Test offset_for with a predicate that matches 'H'
+        assert_eq!(wstream.offset_for(|b| b == b'H'), Some(0));
         assert_eq!(stream.offset_for(|b| b == b'H'), Some(0));
 
         // Test offset_for with a predicate that matches 'l'
+        assert_eq!(wstream.offset_for(|b| b == b'l'), Some(2));
         assert_eq!(stream.offset_for(|b| b == b'l'), Some(2));
 
         // Test offset_for with a predicate that matches 'o'
+        assert_eq!(wstream.offset_for(|b| b == b'o'), Some(4));
         assert_eq!(stream.offset_for(|b| b == b'o'), Some(4));
 
         // Test offset_for with a predicate that matches ','
+        assert_eq!(wstream.offset_for(|b| b == b','), Some(5));
         assert_eq!(stream.offset_for(|b| b == b','), Some(5));
 
         // Test offset_for with a predicate that matches ' '
+        assert_eq!(wstream.offset_for(|b| b == b' '), Some(6));
         assert_eq!(stream.offset_for(|b| b == b' '), Some(6));
 
         // Test offset_for with a predicate that matches 'W'
+        assert_eq!(wstream.offset_for(|b| b == b'W'), Some(7));
         assert_eq!(stream.offset_for(|b| b == b'W'), Some(7));
 
         // Test offset_for with a predicate that matches 'r'
+        assert_eq!(wstream.offset_for(|b| b == b'r'), Some(9));
         assert_eq!(stream.offset_for(|b| b == b'r'), Some(9));
 
         // Test offset_for with a predicate that matches 'd'
+        assert_eq!(wstream.offset_for(|b| b == b'd'), Some(11));
         assert_eq!(stream.offset_for(|b| b == b'd'), Some(11));
 
         // Test offset_for with a predicate that matches '!'
+        assert_eq!(wstream.offset_for(|b| b == b'!'), Some(12));
         assert_eq!(stream.offset_for(|b| b == b'!'), Some(12));
 
         // Test offset_for with a predicate that doesn't match any character
+        assert_eq!(wstream.offset_for(|b| b == b'z'), None);
         assert_eq!(stream.offset_for(|b| b == b'z'), None);
     }
 
     #[test]
-    fn test_vb6_stream() {
+    fn stream() {
+        let mut wstream = b"Hello, World!".as_slice();
         let mut stream = VB6Stream::new(b"Hello, World!");
+
+        assert_eq!(wstream.next_token(), Some(b'H'));
         assert_eq!(stream.next_token(), Some(b'H'));
+
+        assert_eq!(wstream.next_token(), Some(b'e'));
         assert_eq!(stream.next_token(), Some(b'e'));
+
+        assert_eq!(wstream.next_token(), Some(b'l'));
         assert_eq!(stream.next_token(), Some(b'l'));
+
+        assert_eq!(wstream.next_token(), Some(b'l'));
         assert_eq!(stream.next_token(), Some(b'l'));
+
+        assert_eq!(wstream.next_token(), Some(b'o'));
         assert_eq!(stream.next_token(), Some(b'o'));
+
+        assert_eq!(wstream.next_token(), Some(b','));
         assert_eq!(stream.next_token(), Some(b','));
+
+        assert_eq!(wstream.next_token(), Some(b' '));
         assert_eq!(stream.next_token(), Some(b' '));
+
+        assert_eq!(wstream.next_token(), Some(b'W'));
         assert_eq!(stream.next_token(), Some(b'W'));
+
+        assert_eq!(wstream.next_token(), Some(b'o'));
         assert_eq!(stream.next_token(), Some(b'o'));
+
+        assert_eq!(wstream.next_token(), Some(b'r'));
         assert_eq!(stream.next_token(), Some(b'r'));
+
+        assert_eq!(wstream.next_token(), Some(b'l'));
         assert_eq!(stream.next_token(), Some(b'l'));
+
+        assert_eq!(wstream.next_token(), Some(b'd'));
         assert_eq!(stream.next_token(), Some(b'd'));
+
+        assert_eq!(wstream.next_token(), Some(b'!'));
         assert_eq!(stream.next_token(), Some(b'!'));
+
+        assert_eq!(wstream.next_token(), None);
         assert_eq!(stream.next_token(), None);
     }
 
     #[test]
-    fn test_vb6_stream_checkpoint() {
+    fn checkpoint() {
+        let mut wstream = b"Hello, World!".as_slice();
         let mut stream = VB6Stream::new(b"Hello, World!");
 
+        assert_eq!(wstream.next_token(), Some(b'H'));
         assert_eq!(stream.next_token(), Some(b'H'));
+
+        assert_eq!(wstream.next_token(), Some(b'e'));
         assert_eq!(stream.next_token(), Some(b'e'));
+
+        assert_eq!(wstream.next_token(), Some(b'l'));
         assert_eq!(stream.next_token(), Some(b'l'));
+
+        assert_eq!(wstream.next_token(), Some(b'l'));
         assert_eq!(stream.next_token(), Some(b'l'));
+
+        assert_eq!(wstream.next_token(), Some(b'o'));
         assert_eq!(stream.next_token(), Some(b'o'));
+
+        assert_eq!(wstream.next_token(), Some(b','));
         assert_eq!(stream.next_token(), Some(b','));
 
+        let wcheckpoint = wstream.checkpoint();
         let checkpoint = stream.checkpoint();
 
+        assert_eq!(wstream.next_token(), Some(b' '));
         assert_eq!(stream.next_token(), Some(b' '));
+
+        assert_eq!(wstream.next_token(), Some(b'W'));
         assert_eq!(stream.next_token(), Some(b'W'));
+
+        assert_eq!(wstream.next_token(), Some(b'o'));
         assert_eq!(stream.next_token(), Some(b'o'));
+
+        assert_eq!(wstream.next_token(), Some(b'r'));
         assert_eq!(stream.next_token(), Some(b'r'));
+
+        assert_eq!(wstream.next_token(), Some(b'l'));
         assert_eq!(stream.next_token(), Some(b'l'));
+
+        assert_eq!(wstream.next_token(), Some(b'd'));
         assert_eq!(stream.next_token(), Some(b'd'));
+
+        assert_eq!(wstream.next_token(), Some(b'!'));
         assert_eq!(stream.next_token(), Some(b'!'));
+
+        assert_eq!(wstream.next_token(), None);
         assert_eq!(stream.next_token(), None);
 
+        wstream.reset(&wcheckpoint);
         stream.reset(&checkpoint);
 
+        assert_eq!(wstream.next_token(), Some(b' '));
         assert_eq!(stream.next_token(), Some(b' '));
+
+        assert_eq!(wstream.next_token(), Some(b'W'));
         assert_eq!(stream.next_token(), Some(b'W'));
+
+        assert_eq!(wstream.next_token(), Some(b'o'));
         assert_eq!(stream.next_token(), Some(b'o'));
+
+        assert_eq!(wstream.next_token(), Some(b'r'));
         assert_eq!(stream.next_token(), Some(b'r'));
+
+        assert_eq!(wstream.next_token(), Some(b'l'));
         assert_eq!(stream.next_token(), Some(b'l'));
+
+        assert_eq!(wstream.next_token(), Some(b'd'));
         assert_eq!(stream.next_token(), Some(b'd'));
+
+        assert_eq!(wstream.next_token(), Some(b'!'));
         assert_eq!(stream.next_token(), Some(b'!'));
+
+        assert_eq!(wstream.next_token(), None);
         assert_eq!(stream.next_token(), None);
     }
 }
