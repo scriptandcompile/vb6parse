35e1f84eee803874e1740100a7e8e511adcb3d5f
ScriptAndCompile <scriptandcompile@gmail.com>
2024-08-18 00:03:12 -0700

    removed commented code.

--- src/lib.rs
+++ src/lib.rs
@@ -8,10 +8,5 @@ pub use crate::language::VB6ControlCommonInformation;
 pub use crate::language::VB6ControlKind;
 pub use crate::language::VB6Token;
 
-// mod parsers::class;
-// mod parsers::form::VB6FormFile;
-// mod parsers::header::VB6Header;
-// mod parsers::module::VB6ModuleFile;
-
 pub use crate::parsers::vb6;
 pub use crate::parsers::VB6Project;
