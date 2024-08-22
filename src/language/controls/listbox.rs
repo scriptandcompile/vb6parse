4ac8379a4a5389ba43c8994d1401cdc1ae9c6bd9
ScriptAndCompile <scriptandcompile@gmail.com>
2024-08-22 14:53:47 -0700

    moved listbox components to the controls module and listbox.rs

---
+++ src/language/controls/listbox.rs
@@ -0,0 +1,83 @@
+use crate::language::controls::{
+    Appearance, DragMode, MousePointer, MultiSelect, OLEDragMode, OLEDropMode,
+};
+use crate::VB6Color;
+
+#[derive(Debug, PartialEq, Eq, Clone)]
+pub enum ListBoxStyle {
+    Standard = 0,
+    Checkbox = 1,
+}
+
+#[derive(Debug, PartialEq, Eq, Clone)]
+pub struct ListBoxProperties<'a> {
+    pub appearance: Appearance,
+    pub back_color: VB6Color,
+    pub causes_validation: bool,
+    pub columns: u32,
+    pub data_field: &'a str,
+    pub data_format: &'a str,
+    pub data_member: &'a str,
+    pub data_source: &'a str,
+    //pub drag_icon: Option<ImageBuffer>,
+    pub drag_mode: DragMode,
+    pub enabled: bool,
+    pub fore_color: VB6Color,
+    pub height: i32,
+    pub help_context_id: i32,
+    pub integral_height: bool,
+    // pub item_data: Vec<&'a str>,
+    pub left: i32,
+    // pub list: Vec<&'a str>,
+    // pub mouse_icon: Option<ImageBuffer>,
+    pub mouse_pointer: MousePointer,
+    pub multi_select: MultiSelect,
+    pub ole_drag_mode: OLEDragMode,
+    pub ole_drop_mode: OLEDropMode,
+    pub right_to_left: bool,
+    pub sorted: bool,
+    pub style: ListBoxStyle,
+    pub tab_index: i32,
+    pub tab_stop: bool,
+    pub tool_tip_text: &'a str,
+    pub top: i32,
+    pub visible: bool,
+    pub whats_this_help_id: i32,
+    pub width: i32,
+}
+
+impl Default for ListBoxProperties<'_> {
+    fn default() -> Self {
+        ListBoxProperties {
+            appearance: Appearance::ThreeD,
+            back_color: VB6Color::from_hex("&H8000000F&").unwrap(),
+            causes_validation: true,
+            columns: 0,
+            data_field: "",
+            data_format: "",
+            data_member: "",
+            data_source: "",
+            drag_mode: DragMode::Manual,
+            enabled: true,
+            fore_color: VB6Color::from_hex("&H80000012&").unwrap(),
+            height: 30,
+            help_context_id: 0,
+            integral_height: true,
+            left: 30,
+            mouse_pointer: MousePointer::Default,
+            multi_select: MultiSelect::None,
+            ole_drag_mode: OLEDragMode::Manual,
+            ole_drop_mode: OLEDropMode::None,
+            right_to_left: false,
+            sorted: false,
+            style: ListBoxStyle::Standard,
+            tab_index: 0,
+            tab_stop: true,
+            tool_tip_text: "",
+            top: 30,
+            visible: true,
+            whats_this_help_id: 0,
+            width: 100,
+        }
+    }
+}
