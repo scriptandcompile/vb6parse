d6e738ebd7254b326c2087ccf23af8650808f1f9
ScriptAndCompile <scriptandcompile@gmail.com>
2024-08-22 17:11:54 -0700

    moved filelistbox control components to filelistbox.rs

--- src/language/controls/mod.rs
+++ src/language/controls/mod.rs
@@ -1,6 +1,7 @@
 pub mod checkbox;
 pub mod commandbutton;
 pub mod data;
+pub mod filelistbox;
 pub mod frame;
 pub mod label;
 pub mod line;
@@ -15,6 +16,7 @@ use crate::language::{
     controls::checkbox::CheckBoxProperties,
     controls::commandbutton::CommandButtonProperties,
     controls::data::DataProperties,
+    controls::filelistbox::FileListBoxProperties,
     controls::frame::FrameProperties,
     controls::label::LabelProperties,
     controls::line::LineProperties,
@@ -368,38 +370,6 @@ pub enum ShortCut {
     Del,
     ShiftDel,
     AltBKsp,
-}
-
-#[derive(Debug, PartialEq, Eq, Clone)]
-pub struct FileListBoxProperties<'a> {
-    pub appearance: Appearance,
-    pub archive: bool,
-    pub back_color: VB6Color,
-    pub causes_validation: bool,
-    //pub drag_icon: Option<ImageBuffer>,
-    pub drag_mode: DragMode,
-    pub enabled: bool,
-    pub fore_color: VB6Color,
-    pub height: i32,
-    pub help_context_id: i32,
-    pub hidden: bool,
-    pub left: i32,
-    // pub mouse_icon: Option<ImageBuffer>,
-    pub mouse_pointer: MousePointer,
-    pub multi_select: MultiSelect,
-    pub normal: bool,
-    pub ole_drag_mode: OLEDragMode,
-    pub ole_drop_mode: OLEDropMode,
-    pub pattern: &'a str,
-    pub read_only: bool,
-    pub system: bool,
-    pub tab_index: i32,
-    pub tab_stop: bool,
-    pub tool_tip_text: &'a str,
-    pub top: i32,
-    pub visible: bool,
-    pub whats_this_help_id: i32,
-    pub width: i32,
 }
 
 #[derive(Debug, PartialEq, Eq, Clone)]
