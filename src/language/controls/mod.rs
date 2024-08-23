6d0accd366b3c7aed6f2015c77149b8082445db7
ScriptAndCompile <scriptandcompile@gmail.com>
2024-08-22 17:13:53 -0700

    moved drivelistbox control components into drivelistbox.rs

--- src/language/controls/mod.rs
+++ src/language/controls/mod.rs
@@ -1,6 +1,7 @@
 pub mod checkbox;
 pub mod commandbutton;
 pub mod data;
+pub mod drivelistbox;
 pub mod filelistbox;
 pub mod frame;
 pub mod label;
@@ -16,6 +17,7 @@ use crate::language::{
     controls::checkbox::CheckBoxProperties,
     controls::commandbutton::CommandButtonProperties,
     controls::data::DataProperties,
+    controls::drivelistbox::DriveListBoxProperties,
     controls::filelistbox::FileListBoxProperties,
     controls::frame::FrameProperties,
     controls::label::LabelProperties,
@@ -370,30 +372,6 @@ pub enum ShortCut {
     Del,
     ShiftDel,
     AltBKsp,
-}
-
-#[derive(Debug, PartialEq, Eq, Clone)]
-pub struct DriveListBoxProperties<'a> {
-    pub appearance: Appearance,
-    pub back_color: VB6Color,
-    pub causes_validation: bool,
-    //pub drag_icon: Option<ImageBuffer>,
-    pub drag_mode: DragMode,
-    pub enabled: bool,
-    pub fore_color: VB6Color,
-    pub height: i32,
-    pub help_context_id: i32,
-    pub left: i32,
-    // pub mouse_icon: Option<ImageBuffer>,
-    pub mouse_pointer: MousePointer,
-    pub ole_drop_mode: OLEDropMode,
-    pub tab_index: i32,
-    pub tab_stop: bool,
-    pub tool_tip_text: &'a str,
-    pub top: i32,
-    pub visible: bool,
-    pub whats_this_help_id: i32,
-    pub width: i32,
 }
 
 #[derive(Debug, PartialEq, Eq, Clone)]
