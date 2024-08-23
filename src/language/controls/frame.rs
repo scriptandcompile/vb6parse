0f8dc4096c74e22d3158dcec50980ce10e57a0bd
ScriptAndCompile <scriptandcompile@gmail.com>
2024-08-22 17:06:28 -0700

    moved frame control components into frame.rs

---
+++ src/language/controls/frame.rs
@@ -0,0 +1,55 @@
+use crate::language::color::VB6Color;
+use crate::language::controls::{Appearance, BorderStyle, DragMode, MousePointer, OLEDropMode};
+
+#[derive(Debug, PartialEq, Eq, Clone)]
+pub struct FrameProperties<'a> {
+    pub appearance: Appearance,
+    pub back_color: VB6Color,
+    pub border_style: BorderStyle,
+    pub caption: &'a str,
+    pub clip_controls: bool,
+    //pub drag_icon: Option<ImageBuffer>,
+    pub drag_mode: DragMode,
+    pub enabled: bool,
+    pub fore_color: VB6Color,
+    pub height: i32,
+    pub help_context_id: i32,
+    pub left: i32,
+    // pub mouse_icon: Option<ImageBuffer>,
+    pub mouse_pointer: MousePointer,
+    pub ole_drop_mode: OLEDropMode,
+    pub right_to_left: bool,
+    pub tab_index: i32,
+    pub tool_tip_text: &'a str,
+    pub top: i32,
+    pub visible: bool,
+    pub whats_this_help_id: i32,
+    pub width: i32,
+}
+
+impl Default for FrameProperties<'_> {
+    fn default() -> Self {
+        FrameProperties {
+            appearance: Appearance::ThreeD,
+            back_color: VB6Color::from_hex("&H8000000F&").unwrap(),
+            border_style: BorderStyle::FixedSingle,
+            caption: "Frame1",
+            clip_controls: true,
+            drag_mode: DragMode::Manual,
+            enabled: true,
+            fore_color: VB6Color::from_hex("&H80000012&").unwrap(),
+            height: 30,
+            help_context_id: 0,
+            left: 30,
+            mouse_pointer: MousePointer::Default,
+            ole_drop_mode: OLEDropMode::None,
+            right_to_left: false,
+            tab_index: 0,
+            tool_tip_text: "",
+            top: 30,
+            visible: true,
+            whats_this_help_id: 0,
+            width: 100,
+        }
+    }
+}
