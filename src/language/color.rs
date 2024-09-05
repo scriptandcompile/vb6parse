7f0daeafa4f329bccb2881360433360cfa30b7e1
ScriptAndCompile <scriptandcompile@gmail.com>
2024-09-05 15:01:51 -0700

    Clippy: Fixed attribute, docs, and lifetime lints in color.rs

--- src/language/color.rs
+++ src/language/color.rs
@@ -1,9 +1,8 @@
 use crate::errors::VB6ErrorKind;
 
-/// Represents a VB6 color.
-/// The color is represented as a 24-bit RGB value.
-/// The red, green, and blue values are each 8-bits.
-/// This is stored and used within VB6 as text formatted as &H00BBGGRR& value.
+/// `VB6Colors` are 24 bits with 8 bits for red, green, and blue.
+///
+/// `VB6Colors` are stored and used within VB6 as text formatted as '&H00BBGGRR&'.
 /// if, instead, the value begins with '&H80' such as in '&H80000000&', then
 /// the color is a system color. and the value is not the elements of the color,
 /// but rather the index of a system color.
@@ -23,7 +22,7 @@ pub enum VB6Color {
     },
 }
 
-/// A VB6Color with red, green, and blue values of 0x00.
+/// A `VB6Color` with red, green, and blue values of 0x00.
 /// This is the same as calling `VB6Color::new(0x00, 0x00, 0x00)`.
 /// This corresponds to the VB6 color constant `vbBlack`.
 #[allow(dead_code)]
@@ -33,7 +32,7 @@ pub const VB_BLACK: VB6Color = VB6Color::RGB {
     blue: 0x00,
 };
 
-/// A VB6Color with red, green, and blue values of 0xFF.
+/// A `VB6Color` with red, green, and blue values of 0xFF.
 /// This is the same as calling `VB6Color::new(0xFF, 0xFF, 0xFF)`.
 /// This corresponds to the VB6 color constant `vbWhite`.
 #[allow(dead_code)]
@@ -43,7 +42,7 @@ pub const VB_WHITE: VB6Color = VB6Color::RGB {
     blue: 0xFF,
 };
 
-/// A VB6Color with a red value of 0xFF and a green and blue value of 0x00.
+/// A `VB6Color` with a red value of 0xFF and a green and blue value of 0x00.
 /// This is the same as calling `VB6Color::new(0xFF, 0x00, 0x00)`.
 /// This corresponds to the VB6 color constant `vbRed`.
 #[allow(dead_code)]
@@ -53,8 +52,9 @@ pub const VB_RED: VB6Color = VB6Color::RGB {
     blue: 0x00,
 };
 
-/// A VB6Color with a red value of 0x00 and a green value of
+/// A `VB6Color` with a red value of 0x00 and a green value of
 /// 0xFF and a blue value of 0x00.
+///
 /// This is the same as calling `VB6Color::new(0x00, 0xFF, 0x00)`.
 /// This corresponds to the VB6 color constant `vbGreen`.
 #[allow(dead_code)]
@@ -64,7 +64,8 @@ pub const VB_GREEN: VB6Color = VB6Color::RGB {
     blue: 0x00,
 };
 
-/// A VB6Color with red and green values of 0x00 and a blue value of 0xFF.
+/// A `VB6Color` with red and green values of 0x00 and a blue value of 0xFF.
+///
 /// This is the same as calling `VB6Color::new(0x00, 0x00, 0xFF)`.
 /// This corresponds to the VB6 color constant `vbBlue`.
 #[allow(dead_code)]
@@ -74,7 +75,8 @@ pub const VB_BLUE: VB6Color = VB6Color::RGB {
     blue: 0xFF,
 };
 
-/// A VB6Color with a red and green value of 0xFF and a blue value of 0x00.
+/// A `VB6Color` with a red and green value of 0xFF and a blue value of 0x00.
+///
 /// This is the same as calling `VB6Color::new(0xFF, 0xFF, 0x00)`.
 /// This corresponds to the VB6 color constant `vbYellow`.
 #[allow(dead_code)]
@@ -84,7 +86,8 @@ pub const VB_YELLOW: VB6Color = VB6Color::RGB {
     blue: 0x00,
 };
 
-/// A VB6Color with a red and blue value of 0xFF and a green value of 0x00.
+/// A `VB6Color` with a red and blue value of 0xFF and a green value of 0x00.
+///
 /// This is the same as calling `VB6Color::new(0xFF, 0x00, 0xFF)`.
 /// This corresponds to the VB6 color constant `vbMagenta`.
 #[allow(dead_code)]
@@ -94,7 +97,8 @@ pub const VB_MAGENTA: VB6Color = VB6Color::RGB {
     blue: 0xFF,
 };
 
-/// A VB6Color with a red value of 0x00 and a green and blue value of 0xFF.
+/// A `VB6Color` with a red value of 0x00 and a green and blue value of 0xFF.
+///
 /// This is the same as calling `VB6Color::new(0x00, 0xFF, 0xFF)`.
 /// This corresponds to the VB6 color constant `vbCyan`.
 #[allow(dead_code)]
@@ -104,7 +108,7 @@ pub const VB_CYAN: VB6Color = VB6Color::RGB {
     blue: 0xFF,
 };
 
-/// Darkest shadow color for 3-@ display elements.
+/// Darkest shadow color for 3-D display elements.
 #[allow(dead_code)]
 pub const VB_3D_DK_SHADOW: VB6Color = VB6Color::System { index: 0x15 };
 
@@ -176,19 +180,19 @@ pub const VB_INACTIVE_CAPTION_TEXT: VB6Color = VB6Color::System { index: 0x13 };
 #[allow(dead_code)]
 pub const VB_INACTIVE_TITLE_BAR: VB6Color = VB6Color::System { index: 0x03 };
 
-/// Background color of ToolTips
+/// Background color of tool tips
 #[allow(dead_code)]
 pub const VB_INFO_BACKGROUND: VB6Color = VB6Color::System { index: 0x18 };
 
-/// Background color of ToolTips
+/// Background color of tool tips
 #[allow(dead_code)]
 pub const VB_MSG_BOX_TEXT: VB6Color = VB6Color::System { index: 0x18 };
 
-/// Color of text in ToolTips
+/// Color of text in tool tips
 #[allow(dead_code)]
 pub const VB_INFO_TEXT: VB6Color = VB6Color::System { index: 0x17 };
 
-/// Color of text in ToolTips
+/// Color of text in tool tips
 #[allow(dead_code)]
 pub const VB_MSG_BOX: VB6Color = VB6Color::System { index: 0x17 };
 
@@ -221,7 +225,7 @@ pub const VB_WINDOW_FRAME: VB6Color = VB6Color::System { index: 0x06 };
 pub const VB_WINDOW_TEXT: VB6Color = VB6Color::System { index: 0x08 };
 
 impl VB6Color {
-    /// Creates a new VB6Color.
+    /// Creates a new `VB6Color`.
     ///
     /// # Arguments
     ///
@@ -231,12 +235,13 @@ impl VB6Color {
     ///
     /// # Returns
     ///
-    /// A new VB6Color.
+    /// A new RGB `VB6Color`.
+    #[must_use]
     pub fn new(red: u8, green: u8, blue: u8) -> Self {
         VB6Color::RGB { red, green, blue }
     }
 
-    /// Creates a new VB6Color that represents a system color.
+    /// Creates a new `VB6Color` that represents a system color.
     /// The index is the index of the system color.
     /// This is the same as calling `VB6Color::System { index }`.
     ///
@@ -245,14 +250,14 @@ impl VB6Color {
     ///
     /// # Returns
     ///
-    /// A new VB6Color of a system type.
+    /// A new system `VB6Color`.
+    #[must_use]
     pub fn system(index: u8) -> Self {
         VB6Color::System { index }
     }
 
-    /// Creates a new VB6Color with an alpha value of 0xFF.
-    /// This is the same as calling `VB6Color::new(0xFF, red, green, blue)`.
-    /// This is useful when you don't need to specify an alpha value.
+    /// Creates a new `VB6Color` with an RGB value.
+    /// This is the same as calling `VB6Color::new(red, green, blue)`.
     ///
     /// # Arguments
     ///
@@ -262,7 +267,7 @@ impl VB6Color {
     ///
     /// # Returns
     ///
-    /// A new VB6Color.
+    /// A new RGB `VB6Color`.
     ///
     /// # Example
     ///
@@ -271,28 +276,40 @@ impl VB6Color {
     ///
     /// use vb6parse::language::VB6Color;
     ///
-    /// let color = VB6Color::rgb(0xFF, 0x00, 0x00);
+    /// let color = VB6Color::rgb(0xFF, 0x33, 0x12);
     ///
     /// assert!(matches!(color, VB6Color::RGB { .. } ));
-    /// assert_eq!(color, VB6Color::RGB { red: 0xFF, green: 0x00, blue: 0x00 });
+    /// assert_eq!(color, VB6Color::RGB { red: 0xFF, green: 0x33, blue: 0x12 });
     /// ```
+    #[must_use]
     pub fn rgb(red: u8, green: u8, blue: u8) -> Self {
         VB6Color::RGB { red, green, blue }
     }
 
-    /// Parses a VB6 color.
+    /// Parses a `VB6Color`.
     ///
-    /// The color is represented as a 32-bit RGB value.
+    /// The color is represented as a 24-bit RGB value.
     /// The red, green, and blue values are each 8-bits.
-    /// This is stored and used in VB6 as a formatted hex text value &HAABBGGRR&.
+    /// This is stored and used in VB6 as a formatted hex text value &H00BBGGRR&.
     ///
     /// # Arguments
     ///
     /// * `input` - The input to parse.
     ///
+    /// # Errors
+    ///
+    /// If the input is not a valid hex color of either a
+    /// '&H00BBGGRR&' or '&H800000II&' format, an error is returned.
+    ///
+    /// The '&H00BBGGRR&' format is a 24-bit RGB color in the order of blue, green, red.
+    /// where each element is in hex format.
+    ///
+    /// The '&H800000II&' format is a system color with the index value of 'II'.
+    /// Again, where 'II' is in hex format.
+    ///
     /// # Returns
     ///
-    /// The VB6 color.
+    /// The `VB6Color`.
     ///
     /// # Example
     ///
@@ -308,7 +325,7 @@ impl VB6Color {
     /// assert!(matches!(color, VB6Color::RGB { .. } ));
     /// assert_eq!(color, VB6Color::RGB { red: 0xFF, green: 0xCC, blue: 0xBB });
     /// ```
-    pub fn from_hex<'a>(input: &str) -> Result<VB6Color, VB6ErrorKind> {
+    pub fn from_hex(input: &str) -> Result<VB6Color, VB6ErrorKind> {
         let kind_ascii = &input[2..4];
 
         let kind =
