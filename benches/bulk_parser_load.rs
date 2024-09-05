8fbdc20ce8c13e1da6581a68ba228c3b2ffe42fd
ScriptAndCompile <scriptandcompile@gmail.com>
2024-09-05 13:49:32 -0700

    added more benches for forms, modules, and classes.

--- benches/bulk_parser_load.rs
+++ benches/bulk_parser_load.rs
@@ -1,4 +1,5 @@
 use vb6parse::parsers::VB6ClassFile;
+use vb6parse::parsers::VB6ModuleFile;
 use vb6parse::parsers::VB6Project;
 use vb6parse::parsers::VB6Stream;
 
@@ -120,5 +121,144 @@ fn class_benchmark(c: &mut Criterion) {
     });
 }
 
-criterion_group!(benches, project_benchmark, class_benchmark);
+fn bas_module_benchmark(c: &mut Criterion) {
+    let bas_module_names = vec![
+        "Physics_Logic.bas".to_owned(),
+        "mod_PublicVars.bas".to_owned(),
+        "mod_PublicVars.bas".to_owned(),
+        "Declarations.bas".to_owned(),
+        "Subs.bas".to_owned(),
+        "mod_PublicVars.bas".to_owned(),
+        "Declarations.bas".to_owned(),
+    ];
+
+    let bas_modules = vec![
+        include_bytes!("../tests/data/vb6-code/Game-physics-basic/Physics_Logic.bas").to_vec(),
+        include_bytes!("../tests/data/vb6-code/Levels-effect/mod_PublicVars.bas").to_vec(),
+        include_bytes!("../tests/data/vb6-code/Histograms-advanced/mod_PublicVars.bas").to_vec(),
+        include_bytes!("../tests/data/vb6-code/Map-editor-2D/Declarations.bas").to_vec(),
+        include_bytes!("../tests/data/vb6-code/Map-editor-2D/Subs.bas").to_vec(),
+        include_bytes!("../tests/data/vb6-code/Histograms-basic/mod_PublicVars.bas").to_vec(),
+        include_bytes!("../tests/data/vb6-code/Artificial-life/Declarations.bas").to_vec(),
+    ];
+
+    let bas_modules_pairs: Vec<(_, _)> = bas_module_names.iter().zip(bas_modules.iter()).collect();
+
+    c.bench_function("load multiple bas modules", |b| {
+        b.iter(|| {
+            for bas_module_pair in &bas_modules_pairs {
+                black_box({
+                    let _class = VB6ModuleFile::parse(
+                        bas_module_pair.0.to_string(),
+                        &mut bas_module_pair.1.as_slice(),
+                    );
+                });
+            }
+        })
+    });
+}
+
+fn form_benchmark(c: &mut Criterion) {
+    let form_names = vec![
+        "FormPhysics.frm".to_owned(),
+        "Histogram.frm".to_owned(),
+        "Main.frm".to_owned(),
+        "Contrast.frm".to_owned(),
+        "Colorize.frm".to_owned(),
+        "CustomFilters.frm".to_owned(),
+        //"Diffuse.frm".to_owned(),
+        "frmHMM.frm".to_owned(),
+        "Grayscale.frm".to_owned(),
+        "frmScanner.frm".to_owned(),
+        "ShiftColors.frm".to_owned(),
+        "frmFill.frm".to_owned(),
+        //"Threshold.frm".to_owned(),
+        "frmTransparency.frm".to_owned(),
+        "EdgeDetection.frm".to_owned(),
+        "NatureFilters.frm".to_owned(),
+        "FormScreenCapture.frm".to_owned(),
+        "Histogram.frm".to_owned(),
+        "Main.frm".to_owned(),
+        "Sepia.frm".to_owned(),
+        "frmFire.frm".to_owned(),
+        "RandomizationFX.frm".to_owned(),
+        "Brightness3.frm".to_owned(),
+        "Brightness.frm".to_owned(),
+        "Brightness2.frm".to_owned(),
+        "Brightness.frm".to_owned(),
+        "FrmResize.frm".to_owned(),
+        "Main Editor.frm".to_owned(),
+        "Histogram.frm".to_owned(),
+        "Main.frm".to_owned(),
+        "EmbossEngrave.frm".to_owned(),
+        "Blacklight.frm".to_owned(),
+        "Gradient.frm".to_owned(),
+        "Mandelbrot.frm".to_owned(),
+        "Curves.frm".to_owned(),
+        "frmMain.frm".to_owned(),
+    ];
+
+    let forms = vec![
+        include_bytes!("../tests/data/vb6-code/Game-physics-basic/FormPhysics.frm").to_vec(),
+        include_bytes!("../tests/data/vb6-code/Levels-effect/Histogram.frm").to_vec(),
+        include_bytes!("../tests/data/vb6-code/Levels-effect/Main.frm").to_vec(),
+        include_bytes!("../tests/data/vb6-code/Contrast-effect/Contrast.frm").to_vec(),
+        include_bytes!("../tests/data/vb6-code/Colorize-effect/Colorize.frm").to_vec(),
+        include_bytes!("../tests/data/vb6-code/Custom-image-filters/CustomFilters.frm").to_vec(),
+        //include_bytes!("../tests/data/vb6-code/Diffuse-effect/Diffuse.frm").to_vec(),
+        include_bytes!("../tests/data/vb6-code/Hidden-Markov-model/frmHMM.frm").to_vec(),
+        include_bytes!("../tests/data/vb6-code/Grayscale-effect/Grayscale.frm").to_vec(),
+        include_bytes!("../tests/data/vb6-code/Scanner-TWAIN/frmScanner.frm").to_vec(),
+        include_bytes!("../tests/data/vb6-code/Color-shift-effect/ShiftColors.frm").to_vec(),
+        include_bytes!("../tests/data/vb6-code/Fill-image-region/frmFill.frm").to_vec(),
+        //include_bytes!("../tests/data/vb6-code/Threshold-effect/Threshold.frm").to_vec(),
+        include_bytes!("../tests/data/vb6-code/Transparency-2D/frmTransparency.frm").to_vec(),
+        include_bytes!("../tests/data/vb6-code/Edge-detection/EdgeDetection.frm").to_vec(),
+        include_bytes!("../tests/data/vb6-code/Nature-effects/NatureFilters.frm").to_vec(),
+        include_bytes!("../tests/data/vb6-code/Screen-capture/FormScreenCapture.frm").to_vec(),
+        include_bytes!("../tests/data/vb6-code/Histograms-advanced/Histogram.frm").to_vec(),
+        include_bytes!("../tests/data/vb6-code/Histograms-advanced/Main.frm").to_vec(),
+        include_bytes!("../tests/data/vb6-code/Sepia-effect/Sepia.frm").to_vec(),
+        include_bytes!("../tests/data/vb6-code/Fire-effect/frmFire.frm").to_vec(),
+        include_bytes!("../tests/data/vb6-code/Randomize-effects/RandomizationFX.frm").to_vec(),
+        include_bytes!("../tests/data/vb6-code/Brightness-effect/Part 3 - DIBs/Brightness3.frm").to_vec(),
+        include_bytes!("../tests/data/vb6-code/Brightness-effect/Part 4 - Even faster DIBs/Brightness.frm").to_vec(),
+        include_bytes!("../tests/data/vb6-code/Brightness-effect/Part 2 - API - GetPixel and SetPixel/Brightness2.frm").to_vec(),
+        include_bytes!("../tests/data/vb6-code/Brightness-effect/Part 1 - Pure VB6/Brightness.frm").to_vec(),
+        include_bytes!("../tests/data/vb6-code/Map-editor-2D/FrmResize.frm").to_vec(),
+        include_bytes!("../tests/data/vb6-code/Map-editor-2D/Main Editor.frm").to_vec(),
+        include_bytes!("../tests/data/vb6-code/Histograms-basic/Histogram.frm").to_vec(),
+        include_bytes!("../tests/data/vb6-code/Histograms-basic/Main.frm").to_vec(),
+        include_bytes!("../tests/data/vb6-code/Emboss-engrave-effect/EmbossEngrave.frm").to_vec(),
+        include_bytes!("../tests/data/vb6-code/Blacklight-effect/Blacklight.frm").to_vec(),
+        include_bytes!("../tests/data/vb6-code/Gradient-2D/Gradient.frm").to_vec(),
+        include_bytes!("../tests/data/vb6-code/Mandelbrot/Mandelbrot.frm").to_vec(),
+        include_bytes!("../tests/data/vb6-code/Curves-effect/Curves.frm").to_vec(),
+        include_bytes!("../tests/data/vb6-code/Artificial-life/frmMain.frm").to_vec(),
+        
+    ];
+
+    let forms_pairs: Vec<(_, _)> = form_names.iter().zip(forms.iter()).collect();
+
+    c.bench_function("load multiple forms", |b| {
+        b.iter(|| {
+            for form_pair in &forms_pairs {
+                black_box({
+                    let _class = VB6ModuleFile::parse(
+                        form_pair.0.to_string(),
+                        &mut form_pair.1.as_slice(),
+                    );
+                });
+            }
+        })
+    });
+}
+
+criterion_group!(
+    benches,
+    project_benchmark,
+    class_benchmark,
+    bas_module_benchmark,
+    form_benchmark
+);
 criterion_main!(benches);
