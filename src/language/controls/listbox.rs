21e26df3bb135e78f5a433711c84433c62be34ae
ScriptAndCompile <scriptandcompile@gmail.com>
2024-09-23 21:48:18 -0700

    Add support for parsing and loading ListBox data instead of just using Default.

--- src/language/controls/listbox.rs
+++ src/language/controls/listbox.rs
@@ -1,13 +1,23 @@
+use std::collections::HashMap;
+
+use crate::errors::VB6ErrorKind;
+use crate::language::color::VB6Color;
 use crate::language::controls::{
     Appearance, DragMode, MousePointer, MultiSelect, OLEDragMode, OLEDropMode,
 };
-use crate::VB6Color;
+use crate::parsers::form::{
+    build_bool_property, build_color_property, build_i32_property, build_property,
+};
 
+use bstr::BStr;
 use image::DynamicImage;
+use num_enum::TryFromPrimitive;
 use serde::Serialize;
 
-#[derive(Debug, PartialEq, Eq, Clone, serde::Serialize)]
+#[derive(Debug, PartialEq, Eq, Clone, Serialize, Default, TryFromPrimitive)]
+#[repr(i32)]
 pub enum ListBoxStyle {
+    #[default]
     Standard = 0,
     Checkbox = 1,
 }
@@ -23,11 +33,11 @@ pub struct ListBoxProperties<'a> {
     pub appearance: Appearance,
     pub back_color: VB6Color,
     pub causes_validation: bool,
-    pub columns: u32,
-    pub data_field: &'a str,
-    pub data_format: &'a str,
-    pub data_member: &'a str,
-    pub data_source: &'a str,
+    pub columns: i32,
+    pub data_field: &'a BStr,
+    pub data_format: &'a BStr,
+    pub data_member: &'a BStr,
+    pub data_source: &'a BStr,
     pub drag_icon: Option<DynamicImage>,
     pub drag_mode: DragMode,
     pub enabled: bool,
@@ -35,9 +45,9 @@ pub struct ListBoxProperties<'a> {
     pub height: i32,
     pub help_context_id: i32,
     pub integral_height: bool,
-    // pub item_data: Vec<&'a str>,
+    // pub item_data: Vec<&'a BStr>,
     pub left: i32,
-    // pub list: Vec<&'a str>,
+    // pub list: Vec<&'a BStr>,
     pub mouse_icon: Option<DynamicImage>,
     pub mouse_pointer: MousePointer,
     pub multi_select: MultiSelect,
@@ -48,7 +58,7 @@ pub struct ListBoxProperties<'a> {
     pub style: ListBoxStyle,
     pub tab_index: i32,
     pub tab_stop: bool,
-    pub tool_tip_text: &'a str,
+    pub tool_tip_text: &'a BStr,
     pub top: i32,
     pub visible: bool,
     pub whats_this_help_id: i32,
@@ -62,10 +72,10 @@ impl Default for ListBoxProperties<'_> {
             back_color: VB6Color::from_hex("&H8000000F&").unwrap(),
             causes_validation: true,
             columns: 0,
-            data_field: "",
-            data_format: "",
-            data_member: "",
-            data_source: "",
+            data_field: BStr::new(""),
+            data_format: BStr::new(""),
+            data_member: BStr::new(""),
+            data_source: BStr::new(""),
             drag_icon: None,
             drag_mode: DragMode::Manual,
             enabled: true,
@@ -84,7 +94,7 @@ impl Default for ListBoxProperties<'_> {
             style: ListBoxStyle::Standard,
             tab_index: 0,
             tab_stop: true,
-            tool_tip_text: "",
+            tool_tip_text: BStr::new(""),
             top: 30,
             visible: true,
             whats_this_help_id: 0,
@@ -100,7 +110,7 @@ impl Serialize for ListBoxProperties<'_> {
     {
         use serde::ser::SerializeStruct;
 
-        let mut s = serializer.serialize_struct("ListBoxProperties", 26)?;
+        let mut s = serializer.serialize_struct("ListBoxProperties", 31)?;
         s.serialize_field("appearance", &self.appearance)?;
         s.serialize_field("back_color", &self.back_color)?;
         s.serialize_field("causes_validation", &self.causes_validation)?;
@@ -140,5 +150,120 @@ impl Serialize for ListBoxProperties<'_> {
         s.serialize_field("width", &self.width)?;
 
         s.end()
+    }
+}
+
+impl<'a> ListBoxProperties<'a> {
+    pub fn construct_control(
+        properties: &HashMap<&'a BStr, &'a BStr>,
+    ) -> Result<Self, VB6ErrorKind> {
+        let mut list_box_properties = ListBoxProperties::default();
+
+        list_box_properties.appearance =
+            build_property::<Appearance>(properties, BStr::new("Appearance"));
+        list_box_properties.back_color = build_color_property(
+            properties,
+            BStr::new("BackColor"),
+            list_box_properties.back_color,
+        );
+        list_box_properties.causes_validation = build_bool_property(
+            properties,
+            BStr::new("CausesValidation"),
+            list_box_properties.causes_validation,
+        );
+        list_box_properties.columns = build_i32_property(
+            properties,
+            BStr::new("Columns"),
+            list_box_properties.columns,
+        );
+        list_box_properties.data_field = properties
+            .get(BStr::new("DataField"))
+            .unwrap_or(&list_box_properties.data_field);
+        list_box_properties.data_format = properties
+            .get(BStr::new("DataFormat"))
+            .unwrap_or(&list_box_properties.data_format);
+        list_box_properties.data_member = properties
+            .get(BStr::new("DataMember"))
+            .unwrap_or(&list_box_properties.data_member);
+        list_box_properties.data_source = properties
+            .get(BStr::new("DataSource"))
+            .unwrap_or(&list_box_properties.data_source);
+
+        // DragIcon
+
+        list_box_properties.drag_mode =
+            build_property::<DragMode>(properties, BStr::new("DragMode"));
+        list_box_properties.enabled = build_bool_property(
+            properties,
+            BStr::new("Enabled"),
+            list_box_properties.enabled,
+        );
+        list_box_properties.fore_color = build_color_property(
+            properties,
+            BStr::new("ForeColor"),
+            list_box_properties.fore_color,
+        );
+        list_box_properties.height =
+            build_i32_property(properties, BStr::new("Height"), list_box_properties.height);
+        list_box_properties.help_context_id = build_i32_property(
+            properties,
+            BStr::new("HelpContextID"),
+            list_box_properties.help_context_id,
+        );
+        list_box_properties.integral_height = build_bool_property(
+            properties,
+            BStr::new("IntegralHeight"),
+            list_box_properties.integral_height,
+        );
+        list_box_properties.left =
+            build_i32_property(properties, BStr::new("Left"), list_box_properties.left);
+
+        // MouseIcon
+
+        list_box_properties.mouse_pointer =
+            build_property::<MousePointer>(properties, BStr::new("MousePointer"));
+        list_box_properties.multi_select =
+            build_property::<MultiSelect>(properties, BStr::new("MultiSelect"));
+        list_box_properties.ole_drag_mode =
+            build_property::<OLEDragMode>(properties, BStr::new("OLEDragMode"));
+        list_box_properties.ole_drop_mode =
+            build_property::<OLEDropMode>(properties, BStr::new("OLEDropMode"));
+        list_box_properties.right_to_left = build_bool_property(
+            properties,
+            BStr::new("RightToLeft"),
+            list_box_properties.right_to_left,
+        );
+        list_box_properties.sorted =
+            build_bool_property(properties, BStr::new("Sorted"), list_box_properties.sorted);
+        list_box_properties.style = build_property::<ListBoxStyle>(properties, BStr::new("Style"));
+        list_box_properties.tab_index = build_i32_property(
+            properties,
+            BStr::new("TabIndex"),
+            list_box_properties.tab_index,
+        );
+        list_box_properties.tab_stop = build_bool_property(
+            properties,
+            BStr::new("TabStop"),
+            list_box_properties.tab_stop,
+        );
+        list_box_properties.tool_tip_text = properties
+            .get(BStr::new("ToolTipText"))
+            .unwrap_or(&list_box_properties.tool_tip_text);
+        list_box_properties.top =
+            build_i32_property(properties, BStr::new("Top"), list_box_properties.top);
+        list_box_properties.visible = build_bool_property(
+            properties,
+            BStr::new("Visible"),
+            list_box_properties.visible,
+        );
+        list_box_properties.whats_this_help_id = build_i32_property(
+            properties,
+            BStr::new("WhatsThisHelpID"),
+            list_box_properties.whats_this_help_id,
+        );
+        list_box_properties.width =
+            build_i32_property(properties, BStr::new("Width"), list_box_properties.width);
+
+        Ok(list_box_properties)
     }
 }
