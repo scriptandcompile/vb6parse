7577b6cfcde103f7075edc364f260e25b97e3b78
ScriptAndCompile <scriptandcompile@gmail.com>
2025-05-17 18:27:47 -0700

    Added module level documentation for the parsers module.

--- src/parsers/mod.rs
+++ src/parsers/mod.rs
@@ -1,3 +1,24 @@
+//! This module contains the file parsers for the Visual Basic 6 language.
+//!
+//! The major components of this module are:
+//!
+//! `VB6Stream` - The main parsing structure for VB6 files. It holds the
+//! bytes for a file and provides basic methoda for iterating over the bytes as
+//! well as tracking the line, column, and character position of the current byte.
+//!
+//! `VB6Project` - a structure for holding the parsed information from a VB6
+//! project file.
+//!
+//! `VB6ClassFile` - a structure for holding the parsed information from a VB6
+//! class file.
+//!
+//! `VB6FormFile` - a structure for holding the parsed information from a VB6
+//! form file.
+//!
+//! `VB6ModuleFile` - a structure for holding the parsed information from a VB6
+//! module file.
+//!
+
 mod header;
 mod vb6stream;
 
