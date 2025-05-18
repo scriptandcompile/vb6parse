946efb4e445139edd2db779cbe9b8132fb26fd30
ScriptAndCompile <scriptandcompile@gmail.com>
2025-05-18 00:02:19 -0700

    Added documentation for ElseKeyword variant of VB6Token enum.

--- src/language/tokens.rs
+++ src/language/tokens.rs
@@ -299,6 +299,12 @@ pub enum VB6Token<'a> {
     ///
     /// [Reference](https://learn.microsoft.com/en-us/previous-versions/visualstudio/visual-basic-6/aa243382(v=vs.60))
     IfKeyword(&'a BStr),
+    /// Represents the 'Else' keyword.
+    ///
+    /// Used to to conditionally execute a block of code depending on the value
+    /// of an expression.
+    ///
+    /// [Reference](https://learn.microsoft.com/en-us/previous-versions/visualstudio/visual-basic-6/aa243382(v=vs.60))
     ElseKeyword(&'a BStr),
     AndKeyword(&'a BStr),
     OrKeyword(&'a BStr),
