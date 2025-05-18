8154018e68436119f48b55352c77d3d281111cc3
ScriptAndCompile <scriptandcompile@gmail.com>
2025-05-18 16:57:33 -0700

    Added Documentation for and added SavePictureKeyword variant of VB6Token enum.

--- src/parsers/vb6.rs
+++ src/parsers/vb6.rs
@@ -438,6 +438,7 @@ fn vb6_keyword_parse<'a>(input: &mut VB6Stream<'a>) -> VB6Result<VB6Token<'a>> {
             keyword_parse("Case").map(|token: &BStr| VB6Token::CaseKeyword(token)),
             keyword_parse("Seek").map(|token: &BStr| VB6Token::SeekKeyword(token)),
             keyword_parse("SaveSetting").map(|token: &BStr| VB6Token::SaveSettingKeyword(token)),
+            keyword_parse("SavePicture").map(|token: &BStr| VB6Token::SavePictureKeyword(token)),
         )),
     ))
     .parse_next(input)
