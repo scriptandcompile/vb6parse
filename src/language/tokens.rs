e94075494f97403e3c1b5b93f87e135f1f419a46
ScriptAndCompile <scriptandcompile@gmail.com>
2025-05-18 19:30:17 -0700

    Added Documentation for and added DefLngKeyword variant of VB6Token enum.

--- src/language/tokens.rs
+++ src/language/tokens.rs
@@ -754,6 +754,16 @@ pub enum VB6Token<'a> {
     ///
     /// [Reference](https://learn.microsoft.com/en-us/previous-versions/visualstudio/visual-basic-6/aa263421(v=vs.60))
     DefIntKeyword(&'a BStr),
+    /// Represents the 'DefLng' keyword.
+    ///
+    /// Used at module level to set the default data type for variables,
+    /// arguments passed to procedures, and the return type for Function and
+    /// PropertyGet procedures whose names start with the specified characters.
+    ///
+    /// Defaults to the Long data type.
+    ///
+    /// [Reference](https://learn.microsoft.com/en-us/previous-versions/visualstudio/visual-basic-6/aa263421(v=vs.60))
+    DefLngKeyword(&'a BStr),
     NextKeyword(&'a BStr),
 
     /// Represents a dollar sign '$'.
--- src/parsers/vb6.rs
+++ src/parsers/vb6.rs
@@ -478,6 +478,7 @@ fn vb6_keyword_parse<'a>(input: &mut VB6Stream<'a>) -> VB6Result<VB6Token<'a>> {
             keyword_parse("DefBool").map(|token: &BStr| VB6Token::DefBoolKeyword(token)),
             keyword_parse("DefByte").map(|token: &BStr| VB6Token::DefByteKeyword(token)),
             keyword_parse("DefInt").map(|token: &BStr| VB6Token::DefIntKeyword(token)),
+            keyword_parse("DefLng").map(|token: &BStr| VB6Token::DefLngKeyword(token)),
         )),
     ))
     .parse_next(input)
