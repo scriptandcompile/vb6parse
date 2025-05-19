befff37b765f1a0685c6e640ad60e93afc559a71
ScriptAndCompile <scriptandcompile@gmail.com>
2025-05-18 17:07:55 -0700

    Added Documentation for and added ResetKeyword variant of VB6Token enum.

--- src/parsers/vb6.rs
+++ src/parsers/vb6.rs
@@ -442,6 +442,7 @@ fn vb6_keyword_parse<'a>(input: &mut VB6Stream<'a>) -> VB6Result<VB6Token<'a>> {
             keyword_parse("RSet").map(|token: &BStr| VB6Token::RSetKeyword(token)),
             keyword_parse("RmDir").map(|token: &BStr| VB6Token::RmDirKeyword(token)),
             keyword_parse("Resume").map(|token: &BStr| VB6Token::ResumeKeyword(token)),
+            keyword_parse("Reset").map(|token: &BStr| VB6Token::ResetKeyword(token)),
         )),
     ))
     .parse_next(input)
