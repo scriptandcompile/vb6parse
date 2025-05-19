8032000626d38e66f3cefb3249794f2fa2a79edc
ScriptAndCompile <scriptandcompile@gmail.com>
2025-05-18 19:37:22 -0700

    Added Documentation for and added DefDateKeyword variant of VB6Token enum.

--- src/language/tokens.rs
+++ src/language/tokens.rs
@@ -804,6 +804,16 @@ pub enum VB6Token<'a> {
     ///
     /// [Reference](https://learn.microsoft.com/en-us/previous-versions/visualstudio/visual-basic-6/aa263421(v=vs.60))
     DefDecKeyword(&'a BStr),
+    /// Represents the 'DefDate' keyword.
+    ///
+    /// Used at module level to set the default data type for variables,
+    /// arguments passed to procedures, and the return type for Function and
+    /// PropertyGet procedures whose names start with the specified characters.
+    ///
+    /// Defaults to the Date data type.
+    ///
+    /// [Reference](https://learn.microsoft.com/en-us/previous-versions/visualstudio/visual-basic-6/aa263421(v=vs.60))
+    DefDateKeyword(&'a BStr),
     NextKeyword(&'a BStr),
 
     /// Represents a dollar sign '$'.
