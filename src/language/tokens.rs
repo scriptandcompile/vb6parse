15093b8303300f7a658d24248601b5b86a34ace7
ScriptAndCompile <scriptandcompile@gmail.com>
2025-05-18 19:27:45 -0700

    Added Documentation for and added DefByteKeyword variant of VB6Token enum.

--- src/language/tokens.rs
+++ src/language/tokens.rs
@@ -734,6 +734,16 @@ pub enum VB6Token<'a> {
     ///
     /// [Reference](https://learn.microsoft.com/en-us/previous-versions/visualstudio/visual-basic-6/aa263421(v=vs.60))
     DefBoolKeyword(&'a BStr),
+    /// Represents the 'DefByte' keyword.
+    ///
+    /// Used at module level to set the default data type for variables,
+    /// arguments passed to procedures, and the return type for Function and
+    /// PropertyGet procedures whose names start with the specified characters.
+    ///
+    /// Defaults to the Byte data type.
+    ///
+    /// [Reference](https://learn.microsoft.com/en-us/previous-versions/visualstudio/visual-basic-6/aa263421(v=vs.60))
+    DefByteKeyword(&'a BStr),
     NextKeyword(&'a BStr),
 
     /// Represents a dollar sign '$'.
