afa1e546af84737c56e17e6a84ce8a165fe98516
ScriptAndCompile <scriptandcompile@gmail.com>
2025-09-04 14:02:50 -0700

    Lots of spelling fixes in properties.rs

--- src/parsers/class/properties.rs
+++ src/parsers/class/properties.rs
@@ -3,13 +3,14 @@ use serde::Serialize;
 use crate::parsers::header::{VB6FileAttributes, VB6FileFormatVersion};
 
 /// Represents the COM usage of a class file.
-#[derive(Debug, PartialEq, Eq, Clone, Serialize)]
+#[derive(Debug, PartialEq, Eq, Clone, Serialize, Default)]
 pub enum FileUsage {
-    // In a COM object a MultiUse class object will be created for all clients.
-    // This value is stored as -1 (true) in the file.
+    /// In a COM object a MultiUse class object will be created for all clients.
+    /// This value is stored as -1 (true) in the file.
+    #[default]
     MultiUse = -1,
-    // In a COM object a SingleUse class object will be created for each client.
-    // This value is stored as 0 (false) in the file.
+    /// In a COM object a SingleUse class object will be created for each client.
+    /// This value is stored as 0 (false) in the file.
     SingleUse = 0, // 0 (false)
 }
 
@@ -25,13 +26,14 @@ pub enum FileUsage {
 /// added to the class module.
 ///
 /// Without these procedures, the class cannot be saved to disk.
-#[derive(Debug, PartialEq, Eq, Clone, Serialize)]
-pub enum Persistance {
-    // The class property cannot be saved to a file in a property bag.
-    // This value is stored as 0 (false) in the file.
-    NonPersistable = 0,
-    // The class property can be saved to a file in a property bag.
-    // This value is stored as -1 (true) in the file.
+#[derive(Debug, PartialEq, Eq, Clone, Serialize, Default)]
+pub enum Persistence {
+    /// The class property cannot be saved to a file in a property bag.
+    /// This value is stored as 0 (false) in the file.
+    #[default]
+    NotPersistable = 0,
+    /// The class property can be saved to a file in a property bag.
+    /// This value is stored as -1 (true) in the file.
     Persistable = -1,
 }
 
@@ -43,62 +45,63 @@ pub enum Persistance {
 ///
 /// Maps directly to the MTS transaction mode attribute in Microsoft Transaction
 /// Server.
-#[derive(Debug, PartialEq, Eq, Clone, Serialize)]
+#[derive(Debug, PartialEq, Eq, Clone, Serialize, Default)]
 pub enum MtsStatus {
-    // This class is not an MTS component.
-    // This value is stored as 0 in the file.
-    // This is the default value.
+    /// This class is not an MTS component.
+    /// This value is stored as 0 in the file.
+    /// This is the default value.
+    #[default]
     NotAnMTSObject = 0,
-    // This class is an MTS component but does not support transactions.
-    // This value is stored as 1 in the file.
+    /// This class is an MTS component but does not support transactions.
+    /// This value is stored as 1 in the file.
     NoTransactions = 1,
-    // This class is an MTS component and requires a transaction.
-    // This value is stored as 2 in the file.
+    /// This class is an MTS component and requires a transaction.
+    /// This value is stored as 2 in the file.
     RequiresTransaction = 2,
-    // This class is an MTS component and uses a transaction.
-    // This value is stored as 3 in the file.
+    /// This class is an MTS component and uses a transaction.
+    /// This value is stored as 3 in the file.
     UsesTransaction = 3,
-    // This class is an MTS component and requires a new transaction.
-    // This value is stored as 4 in the file.
+    /// This class is an MTS component and requires a new transaction.
+    /// This value is stored as 4 in the file.
     RequiresNewTransaction = 4,
 }
 
-#[derive(Debug, PartialEq, Eq, Clone, Serialize)]
+/// Determines if a class can act as a DataSource for VB6 DataBinding.
+#[derive(Debug, PartialEq, Eq, Clone, Serialize, Default)]
 pub enum DataSourceBehavior {
-    // The class does not support acting as a Data Source.
-    // This value is stored as 0 in the file.
+    /// The class does not support acting as a Data Source.
+    /// This value is stored as 0 in the file.
+    #[default]
     None = 0,
-    // The class supports acting as a Data Source.
-    // This value is stored as 1 in the file.
+    /// The class supports acting as a Data Source.
+    /// This value is stored as 1 in the file.
     DataSource = 1,
 }
 
-#[derive(Debug, PartialEq, Eq, Clone, Serialize)]
+/// Determines the default VB6 DataBinding behavior.
+#[derive(Debug, PartialEq, Eq, Clone, Serialize, Default)]
 pub enum DataBindingBehavior {
-    // The class does not support data binding.
-    // This value is stored as 0 in the file.
+    /// The class does not support data binding.
+    /// This value is stored as 0 in the file.
+    #[default]
     None = 0,
-    // The class supports simple data binding.
-    // This value is stored as 1 in the file.
+    /// The class supports simple data binding.
+    /// This value is stored as 1 in the file.
     Simple = 1,
-    // The class supports complex data binding.
-    // This value is stored as 2 in the file.
+    /// The class supports complex data binding.
+    /// This value is stored as 2 in the file.
     Complex = 2,
 }
 
 /// The properties of a VB6 class file is the list of key/value pairs
 /// found between the BEGIN and END lines in the header.
-#[derive(Debug, PartialEq, Eq, Clone, Serialize)]
+#[derive(Debug, PartialEq, Eq, Clone, Serialize, Default)]
 pub struct VB6ClassProperties {
-    // (0/-1) multi use / single use
+    ///
     pub multi_use: FileUsage,
-    // (0/1) NonParsistable / Persistable
-    pub persistable: Persistance,
-    // (0/1/2) vbNone / vbSimple / vbComplex
+    pub persistable: Persistence,
     pub data_binding_behavior: DataBindingBehavior,
-    // (0/1) vbNone / vbDataSource
     pub data_source_behavior: DataSourceBehavior,
-    // (0/1/2/3/4) NotAnMTSObject / NoTransactions / RequiresTransaction / UsesTransaction / RequiresNewTransaction
     pub mts_transaction_mode: MtsStatus,
 }
 
@@ -108,6 +111,18 @@ pub struct VB6ClassProperties {
 pub struct VB6ClassVersion {
     pub major: u8,
     pub minor: u8,
+}
+
+impl Default for VB6ClassVersion {
+    /// Technically, these could be just about any value and it looks to be a legacy
+    /// file format version number system that Microsoft planned for in order to be
+    /// able to upgrade from format to format without breaking anything.
+    ///
+    /// In practice, this appears to be the only valid version that Microsoft
+    /// implemented.
+    fn default() -> Self {
+        VB6ClassVersion { major: 1, minor: 0 }
+    }
 }
 
 /// Represents the header of a VB6 class file.
