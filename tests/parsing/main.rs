41ce46d25af5b6d157628f8516b6b4cfb5d30a46
ScriptAndCompile <scriptandcompile@gmail.com>
2025-11-16 23:52:32 -0800

    Moved all the cst/parsing related tests into a parsing sub folder to make it easier to organize.

---
+++ tests/parsing/main.rs
@@ -0,0 +1,15 @@
+// This whole file is just a horrible hack to allow grouping of the CST parsing tests into
+// a separate folder without needing to throw all the tests into a single huge file or
+// throwing all test files in the root tests folder.
+
+mod call;
+mod comments;
+mod for_loop;
+mod do_loop;
+mod set_statement;
+mod conditional;
+mod cst_navigation;
+mod cst;
+mod declaration;
+mod option_explicit;
+mod sub;
