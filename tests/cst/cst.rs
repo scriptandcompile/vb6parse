f404fb347213198773dc76972573ae281cb051a0
ScriptAndCompile <scriptandcompile@gmail.com>
2025-11-19 12:07:25 -0800

    moved unit tests to mod.rs and removed them from the integration test areas.

--- tests/cst/cst.rs
+++ tests/cst/cst.rs
@@ -1,52 +1,11 @@
 //! Integration test for CST parsing functionality
 
-use vb6parse::language::VB6Token;
 use vb6parse::parsers::{ConcreteSyntaxTree, SyntaxKind};
-
-#[test]
-fn syntax_kind_conversions() {
-    // Test keyword conversions
-    assert_eq!(
-        SyntaxKind::from(VB6Token::FunctionKeyword),
-        SyntaxKind::FunctionKeyword
-    );
-    assert_eq!(SyntaxKind::from(VB6Token::IfKeyword), SyntaxKind::IfKeyword);
-    assert_eq!(
-        SyntaxKind::from(VB6Token::ForKeyword),
-        SyntaxKind::ForKeyword
-    );
-
-    // Test operators
-    assert_eq!(
-        SyntaxKind::from(VB6Token::AdditionOperator),
-        SyntaxKind::AdditionOperator
-    );
-    assert_eq!(
-        SyntaxKind::from(VB6Token::EqualityOperator),
-        SyntaxKind::EqualityOperator
-    );
-
-    // Test literals
-    assert_eq!(
-        SyntaxKind::from(VB6Token::StringLiteral),
-        SyntaxKind::StringLiteral
-    );
-    assert_eq!(SyntaxKind::from(VB6Token::Number), SyntaxKind::Number);
-}
-
-#[test]
-fn parse_empty_stream() {
-    let source = "";
-        let cst = ConcreteSyntaxTree::from_source("test.bas", source).unwrap();
-
-    assert_eq!(cst.root_kind(), SyntaxKind::Root);
-    assert_eq!(cst.child_count(), 0);
-}
 
 #[test]
 fn parse_attribute_statement() {
     let source = "Attribute VB_Name = \"modTest\"\n";
-        let cst = ConcreteSyntaxTree::from_source("test.bas", source).unwrap();
+    let cst = ConcreteSyntaxTree::from_source("test.bas", source).unwrap();
 
     assert_eq!(cst.root_kind(), SyntaxKind::Root);
     assert_eq!(cst.child_count(), 1); // AttributeStatement
