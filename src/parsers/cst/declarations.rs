d15bef7fd7634477f98a7a28c9af21dbe18ed90b
ScriptAndCompile <scriptandcompile@gmail.com>
2025-11-18 18:33:53 -0800

    Broke up the cst.rs file into the cst module with relevant parse functions into their own files. Should be easy to read and understand.

---
+++ src/parsers/cst/declarations.rs
@@ -0,0 +1,382 @@
+//! Declaration parsing for VB6 CST.
+//!
+//! This module handles parsing of VB6 declarations:
+//! - Function statements
+//! - Sub statements
+//! - Property statements (Get, Let, Set)
+//! - Variable declarations (Dim, Private, Public, Const, Static)
+//! - Parameter lists
+
+use crate::language::VB6Token;
+use crate::parsers::SyntaxKind;
+
+use super::Parser;
+
+impl<'a> Parser<'a> {
+    /// Parse a Visual Basic 6 subroutine with syntax:
+    ///
+    /// \[ Public | Private | Friend \] \[ Static \] Sub name \[ ( arglist ) \]
+    /// \[ statements \]
+    /// \[ Exit Sub \]
+    /// \[ statements \]
+    /// End Sub
+    ///
+    /// The Sub statement syntax has these parts:
+    ///
+    /// | Part        | Optional / Required | Description |
+    /// |-------------|---------------------|-------------|
+    /// | Public   	  | Optional | Indicates that the Sub procedure is accessible to all other procedures in all modules. If used in a module that contains an Option Private statement, the procedure is not available outside the project. |
+    /// | Private  	  | Optional | Indicates that the Sub procedure is accessible only to other procedures in the module where it is declared. |
+    /// | Friend 	  | Optional | Used only in a class module. Indicates that the Sub procedure is visible throughout the project, but not visible to a controller of an instance of an object. |
+    /// | Static 	  | Optional | Indicates that the Sub procedure's local variables are preserved between calls. The Static attribute doesn't affect variables that are declared outside the Sub, even if they are used in the procedure. |
+    /// | name 	      | Required | Name of the Sub; follows standard variable naming conventions. |
+    /// | arglist 	  | Optional | List of variables representing arguments that are passed to the Sub procedure when it is called. Multiple variables are separated by commas. |
+    /// | statements  | Optional | Any group of statements to be executed within the Sub procedure.
+    ///
+    /// The arglist argument has the following syntax and parts:
+    ///
+    /// \[ Optional \] \[ ByVal | ByRef \] \[ ParamArray \] varname \[ ( ) \] \[ As type \] \[ = defaultvalue \]
+    ///
+    /// [Reference](https://learn.microsoft.com/en-us/office/vba/language/reference/user-interface-help/sub-statement)
+    pub(super) fn parse_sub_statement(&mut self) {
+        // if we are now parsing a sub statement, we are no longer in the header.
+        self.parsing_header = false;
+        self.builder.start_node(SyntaxKind::SubStatement.to_raw());
+
+        // Consume optional Public/Private/Friend keyword
+        if self.at_token(VB6Token::PublicKeyword)
+            || self.at_token(VB6Token::PrivateKeyword)
+            || self.at_token(VB6Token::FriendKeyword)
+        {
+            self.consume_token();
+
+            // Consume any whitespace after visibility modifier
+            self.consume_whitespace();
+        }
+
+        // Consume optional Static keyword
+        if self.at_token(VB6Token::StaticKeyword) {
+            self.consume_token();
+
+            // Consume any whitespace after Static
+            self.consume_whitespace();
+        }
+
+        // Consume "Sub" keyword
+        self.consume_token();
+
+        // Consume any whitespace after "Sub"
+        self.consume_whitespace();
+
+        // Consume procedure name
+        if self.at_token(VB6Token::Identifier) {
+            self.consume_token();
+        }
+
+        // Consume any whitespace before parameter list
+        self.consume_whitespace();
+
+        // Parse parameter list if present
+        if self.at_token(VB6Token::LeftParentheses) {
+            self.parse_parameter_list();
+        }
+
+        // Consume everything until newline (preserving all tokens)
+        self.consume_until(VB6Token::Newline);
+
+        // Consume the newline
+        if self.at_token(VB6Token::Newline) {
+            self.consume_token();
+        }
+
+        // Parse body until "End Sub"
+        self.parse_code_block(|parser| {
+            parser.at_token(VB6Token::EndKeyword)
+                && parser.peek_next_keyword() == Some(VB6Token::SubKeyword)
+        });
+
+        // Consume "End Sub" and trailing tokens
+        if self.at_token(VB6Token::EndKeyword) {
+            // Consume "End"
+            self.consume_token();
+
+            // Consume any whitespace between "End" and "Sub"
+            self.consume_whitespace();
+            
+            // Consume "Sub"
+            self.consume_token();
+
+            // Consume until newline (including it)
+            self.consume_until(VB6Token::Newline);
+
+            // Consume the newline
+            if self.at_token(VB6Token::Newline) {
+                self.consume_token();
+            }
+        }
+
+        self.builder.finish_node(); // SubStatement
+    }
+
+    /// Parse a Visual Basic 6 function with syntax:
+    ///
+    /// \[ Public | Private | Friend \] \[ Static \] Function name \[ ( arglist ) \] \[ As type \]
+    /// \[ statements \]
+    /// \[ name = expression \]
+    /// \[ Exit Function \]
+    /// \[ statements \]
+    /// \[ name = expression \]
+    /// End Function
+    ///
+    /// The Function statement syntax has these parts:
+    ///
+    /// | Part        | Optional / Required | Description |
+    /// |-------------|---------------------|-------------|
+    /// | Public   	  | Optional | Indicates that the Function procedure is accessible to all other procedures in all modules. If used in a module that contains an Option Private, the procedure is not available outside the project. |
+    /// | Private  	  | Optional | Indicates that the Function procedure is accessible only to other procedures in the module where it is declared. |
+    /// | Friend 	  | Optional | Used only in a class module. Indicates that the Function procedure is visible throughout the project, but not visible to a controller of an instance of an object. |
+    /// | Static 	  | Optional | Indicates that the Function procedure's local variables are preserved between calls. The Static attribute doesn't affect variables that are declared outside the Function, even if they are used in the procedure. |
+    /// | name 	      | Required | Name of the Function; follows standard variable naming conventions. |
+    /// | arglist 	  | Optional | List of variables representing arguments that are passed to the Function procedure when it is called. Multiple variables are separated by commas. |
+    /// | type 	      | Optional | Data type of the value returned by the Function procedure; may be Byte, Boolean, Integer, Long, Currency, Single, Double, Decimal (not currently supported), Date, String (except fixed length), Object, Variant, or any user-defined type. |
+    /// | statements  | Optional | Any group of statements to be executed within the Function procedure.
+    /// | expression  | Optional | Return value of the Function. |
+    ///
+    /// The arglist argument has the following syntax and parts:
+    ///
+    /// \[ Optional \] \[ ByVal | ByRef \] \[ ParamArray \] varname \[ ( ) \] \[ As type \] \[ = defaultvalue \]
+    ///
+    /// [Reference](https://learn.microsoft.com/en-us/office/vba/language/reference/user-interface-help/function-statement)
+    pub(super) fn parse_function_statement(&mut self) {
+        // if we are now parsing a function statement, we are no longer in the header.
+        self.parsing_header = false;
+
+        self.builder
+            .start_node(SyntaxKind::FunctionStatement.to_raw());
+
+        // Consume optional Public/Private/Friend keyword
+        if self.at_token(VB6Token::PublicKeyword)
+            || self.at_token(VB6Token::PrivateKeyword)
+            || self.at_token(VB6Token::FriendKeyword)
+        {
+            self.consume_token();
+
+            // Consume any whitespace after visibility modifier
+            self.consume_whitespace();
+        }
+
+        // Consume optional Static keyword
+        if self.at_token(VB6Token::StaticKeyword) {
+            self.consume_token();
+
+            // Consume any whitespace after Static
+            self.consume_whitespace();
+        }
+
+        // Consume "Function" keyword
+        self.consume_token();
+
+        // Consume any whitespace after "Function"
+        self.consume_whitespace();
+
+        // Consume function name
+        if self.at_token(VB6Token::Identifier) {
+            self.consume_token();
+        }
+
+        // Consume any whitespace before parameter list
+        self.consume_whitespace();
+
+        // Parse parameter list if present
+        if self.at_token(VB6Token::LeftParentheses) {
+            self.parse_parameter_list();
+        }
+
+        // Consume everything until newline (includes "As Type" if present)
+        self.consume_until(VB6Token::Newline);
+
+        // Consume the newline
+        if self.at_token(VB6Token::Newline) {
+            self.consume_token();
+        }
+
+        // Parse body until "End Function"
+        self.parse_code_block(|parser| {
+            parser.at_token(VB6Token::EndKeyword)
+                && parser.peek_next_keyword() == Some(VB6Token::FunctionKeyword)
+        });
+
+        // Consume "End Function" and trailing tokens
+        if self.at_token(VB6Token::EndKeyword) {
+            // Consume "End"
+            self.consume_token();
+
+            // Consume any whitespace between "End" and "Function"
+            self.consume_whitespace();
+
+            // Consume "Function"
+            self.consume_token();
+
+            // Consume until newline (including it)
+            self.consume_until(VB6Token::Newline);
+
+            // Consume the newline
+            if self.at_token(VB6Token::Newline) {
+                self.consume_token();
+            }
+        }
+
+        self.builder.finish_node(); // FunctionStatement
+    }
+
+    /// Parse a Property statement (Property Get, Property Let, or Property Set).
+    ///
+    /// VB6 Property statement syntax:
+    /// - [Public | Private | Friend] [Static] Property Get name [(arglist)] [As type]
+    /// - [Public | Private | Friend] [Static] Property Let name ([arglist,] value)
+    /// - [Public | Private | Friend] [Static] Property Set name ([arglist,] value)
+    ///
+    /// [Reference](https://learn.microsoft.com/en-us/office/vba/language/reference/user-interface-help/property-get-statement)
+    /// [Reference](https://learn.microsoft.com/en-us/office/vba/language/reference/user-interface-help/property-let-statement)
+    /// [Reference](https://learn.microsoft.com/en-us/office/vba/language/reference/user-interface-help/property-set-statement)
+    pub(super) fn parse_property_statement(&mut self) {
+        // if we are now parsing a property statement, we are no longer in the header.
+        self.parsing_header = false;
+
+        self.builder
+            .start_node(SyntaxKind::PropertyStatement.to_raw());
+
+        // Consume optional Public/Private/Friend keyword
+        if self.at_token(VB6Token::PublicKeyword)
+            || self.at_token(VB6Token::PrivateKeyword)
+            || self.at_token(VB6Token::FriendKeyword)
+        {
+            self.consume_token();
+
+            // Consume any whitespace after visibility modifier
+            self.consume_whitespace();
+        }
+
+        // Consume optional Static keyword
+        if self.at_token(VB6Token::StaticKeyword) {
+            self.consume_token();
+
+            // Consume any whitespace after Static
+            self.consume_whitespace();
+        }
+
+        // Consume "Property" keyword
+        self.consume_token();
+
+        // Consume any whitespace after "Property"
+        self.consume_whitespace();
+
+        // Consume Get/Let/Set keyword
+        if self.at_token(VB6Token::GetKeyword)
+            || self.at_token(VB6Token::LetKeyword)
+            || self.at_token(VB6Token::SetKeyword)
+        {
+            self.consume_token();
+        }
+
+        // Consume any whitespace after Get/Let/Set
+        self.consume_whitespace();
+
+        // Consume property name
+        if self.at_token(VB6Token::Identifier) {
+            self.consume_token();
+        }
+
+        // Consume any whitespace before parameter list
+        self.consume_whitespace();
+
+        // Parse parameter list if present
+        if self.at_token(VB6Token::LeftParentheses) {
+            self.parse_parameter_list();
+        }
+
+        // Consume everything until newline (includes "As Type" if present)
+        self.consume_until(VB6Token::Newline);
+
+        // Consume the newline
+        if self.at_token(VB6Token::Newline) {
+            self.consume_token();
+        }
+
+        // Parse body until "End Property"
+        self.parse_code_block(|parser| {
+            parser.at_token(VB6Token::EndKeyword)
+                && parser.peek_next_keyword() == Some(VB6Token::PropertyKeyword)
+        });
+
+        // Consume "End Property" and trailing tokens
+        if self.at_token(VB6Token::EndKeyword) {
+            // Consume "End"
+            self.consume_token();
+
+            // Consume any whitespace between "End" and "Property"
+            self.consume_whitespace();
+
+            // Consume "Property"
+            self.consume_token();
+
+            // Consume until newline (including it)
+            self.consume_until(VB6Token::Newline);
+
+            // Consume the newline
+            if self.at_token(VB6Token::Newline) {
+                self.consume_token();
+            }
+        }
+
+        self.builder.finish_node(); // PropertyStatement
+    }
+
+    /// Parse a parameter list: (param1 As Type, param2 As Type)
+    pub(super) fn parse_parameter_list(&mut self) {
+        self.builder.start_node(SyntaxKind::ParameterList.to_raw());
+
+        // Consume "("
+        self.consume_token();
+
+        // Consume everything until ")"
+        let mut depth = 1;
+        while !self.is_at_end() && depth > 0 {
+            if self.at_token(VB6Token::LeftParentheses) {
+                depth += 1;
+            } else if self.at_token(VB6Token::RightParentheses) {
+                depth -= 1;
+            }
+
+            self.consume_token();
+
+            if depth == 0 {
+                break;
+            }
+        }
+
+        self.builder.finish_node(); // ParameterList
+    }
+
+    /// Parse a declaration: Dim/Private/Public x As Type
+    pub(super) fn parse_declaration(&mut self) {
+        
+        // if we are now parsing a declaration, we are no longer in the header.
+        self.parsing_header = false;
+        
+        self.builder.start_node(SyntaxKind::DimStatement.to_raw());
+
+        // Consume the keyword (Dim, Private, Public, etc.)
+        self.consume_token();
+
+        // Consume everything until newline (preserving all tokens)
+        self.consume_until(VB6Token::Newline);
+
+        // Consume the newline
+        if self.at_token(VB6Token::Newline) {
+            self.consume_token();
+        }
+
+        self.builder.finish_node(); // DimStatement
+    }
+}
---
+++ src/parsers/cst/helpers.rs
@@ -0,0 +1,215 @@
+//! Helper and utility methods for the CST parser.
+//!
+//! This module contains various helper methods used throughout the parsing process,
+//! including token peeking, consumption, and logical line detection.
+
+use std::num::NonZeroUsize;
+use crate::language::VB6Token;
+use crate::parsers::SyntaxKind;
+use super::Parser;
+
+impl<'a> Parser<'a> {
+    /// Check if we've reached the end of the token stream.
+    pub(super) fn is_at_end(&self) -> bool {
+        self.pos >= self.tokens.len()
+    }
+
+    /// Get the current token without advancing the position.
+    pub(super) fn current_token(&self) -> Option<&VB6Token> {
+        self.tokens.get(self.pos).map(|(_, token)| token)
+    }
+
+    /// Check if the current token matches the given token type.
+    pub(super) fn at_token(&self, token: VB6Token) -> bool {
+        self.current_token() == Some(&token)
+    }
+
+    /// Peek ahead to get the next keyword (non-whitespace token).
+    pub(super) fn peek_next_keyword(&self) -> Option<VB6Token> {
+        self.peek_next_count_keywords(NonZeroUsize::new(1).unwrap()).next()
+    }
+
+    /// Check if the current token is an identifier.
+    pub(super) fn is_identifier(&self) -> bool {
+        matches!(self.current_token(), Some(VB6Token::Identifier))
+    }
+
+    /// Check if the current token is a keyword.
+    pub(super) fn is_keyword(&self) -> bool {
+        match self.current_token() {
+            Some(token) => token.is_keyword(),
+            None => false,
+        }
+    }
+
+    /// Check if the current token is a number.
+    pub(super) fn is_number(&self) -> bool {
+        matches!(self.current_token(), Some(VB6Token::Number))
+    }
+
+    /// Check if we're at the end of a logical line (newline that's NOT a line continuation)
+    /// In VB6, `_` followed by zero or more whitespaces and then a newline means "continue on next line"
+    /// 
+    /// This function primarily looks forward without backtracking, except when positioned
+    /// directly at a newline (which requires checking backward for a preceding underscore):
+    /// - Scans forward through whitespace  
+    /// - If underscore found, checks forward for newline (no backtracking)
+    /// - If newline found directly, checks backward for underscore (minimal backtracking)
+    /// - Otherwise returns false (not at line end)
+    pub(super) fn is_at_logical_line_end(&self) -> bool {
+        let mut check_pos = self.pos;
+        
+        // Skip forward over any whitespace
+        while let Some((_, token)) = self.tokens.get(check_pos) {
+            match token {
+                VB6Token::Whitespace => {
+                    check_pos += 1;
+                }
+                VB6Token::Underscore => {
+                    // Found underscore - check forward if it's followed by whitespace* + newline
+                    let mut after_underscore = check_pos + 1;
+                    
+                    // Skip whitespace after underscore
+                    while let Some((_, ws_token)) = self.tokens.get(after_underscore) {
+                        if *ws_token == VB6Token::Whitespace {
+                            after_underscore += 1;
+                        } else {
+                            break;
+                        }
+                    }
+                    
+                    // Check if we found a newline after the underscore (and optional whitespace)
+                    if let Some((_, next_token)) = self.tokens.get(after_underscore) {
+                        if *next_token == VB6Token::Newline {
+                            // This is a line continuation (underscore + whitespace* + newline)
+                            return false;
+                        }
+                    }
+                    
+                    // Underscore not followed by newline - not a line continuation
+                    return false;
+                }
+                VB6Token::Newline => {
+                    // Found newline directly - must check backward for preceding underscore
+                    // This is the only case requiring backtracking, when already positioned at/near newline
+                    let mut back_pos = self.pos;
+                    
+                    // Skip backward over whitespace
+                    while back_pos > 0 {
+                        back_pos -= 1;
+                        if let Some((_, back_token)) = self.tokens.get(back_pos) {
+                            match back_token {
+                                VB6Token::Whitespace => continue,
+                                VB6Token::Underscore => return false, // Line continuation
+                                _ => return true, // Logical line end
+                            }
+                        }
+                    }
+                    
+                    // Newline at start of file
+                    return true;
+                }
+                _ => {
+                    // Hit a non-whitespace, non-underscore, non-newline token
+                    // Not at end of line
+                    return false;
+                }
+            }
+        }
+        
+        // End of file
+        false
+    }
+
+    /// Peek ahead and get the next `count` non-whitespace keywords from the current position.
+    ///
+    /// # Arguments
+    /// * `count` - Number of keywords to peek ahead (must be non-zero)
+    ///
+    /// # Returns
+    /// An iterator over the next `count` keywords (non-whitespace tokens)
+    pub(super) fn peek_next_count_keywords(&self, count: NonZeroUsize) -> impl Iterator<Item = VB6Token> + '_ {
+        self.tokens
+            .iter()
+            .skip(self.pos + 1)
+            .filter(|(_, token)| *token != VB6Token::Whitespace)
+            .take(count.get())
+            .map(|(_, token)| *token)
+    }
+
+    /// Peek ahead and get the next `count` tokens (including whitespace) from the current position.
+    pub(super) fn peek_next_count_tokens(&self, count: NonZeroUsize) -> impl Iterator<Item = VB6Token> + '_ {
+        self.tokens
+            .iter()
+            .skip(self.pos + 1)
+            .take(count.get())
+            .map(|(_, token)| *token)
+    }
+
+    /// Peek ahead to get the next token (including whitespace).
+    pub(super) fn peek_next_token(&self) -> Option<VB6Token> {
+        self.peek_next_count_tokens(NonZeroUsize::new(1).unwrap()).next()
+    }
+
+    /// Consume the current token and advance to the next position.
+    pub(super) fn consume_token(&mut self) {
+        if let Some((text, token)) = self.tokens.get(self.pos) {
+            let kind = SyntaxKind::from(*token);
+            self.builder.token(kind.to_raw(), text);
+            self.pos += 1;
+        }
+    }
+
+    /// Consume all whitespace tokens at the current position.
+    pub(super) fn consume_whitespace(&mut self) {
+        while self.at_token(VB6Token::Whitespace) {
+            self.consume_token();
+        }
+    }
+
+    /// Consume the current token as an Unknown token.
+    pub(super) fn consume_token_as_unknown(&mut self) {
+        if let Some((text, _)) = self.tokens.get(self.pos) {
+            self.builder.token(SyntaxKind::Unknown.to_raw(), text);
+            self.pos += 1;
+        }
+    }
+
+    /// Consume tokens until reaching the specified token or the end of input.
+    /// The specified token is NOT consumed.
+    ///
+    /// Handles line continuations when consuming until a newline.
+    ///
+    /// # Arguments
+    /// * `target` - The token to stop at (will not be consumed)
+    pub(super) fn consume_until(&mut self, target: VB6Token) {
+        while !self.is_at_end() && !self.at_token(target) {
+            self.consume_token();
+        }
+        
+        // If we're looking for a newline and we found one, check for line continuation
+        // In VB6, underscore followed by whitespace and newline means "continue on next line"
+        if target == VB6Token::Newline && self.at_token(VB6Token::Newline) {
+            // Look back to see if there was an underscore before this newline
+            // We need to check if the last non-whitespace token was an underscore
+            let mut check_pos = self.pos;
+            while check_pos > 0 {
+                check_pos -= 1;
+                if let Some((_, token)) = self.tokens.get(check_pos) {
+                    match token {
+                        VB6Token::Whitespace => continue, // Skip whitespace
+                        VB6Token::Underscore => {
+                            // Found line continuation! Consume the newline and keep going
+                            self.consume_token(); // Consume the newline
+                            // Continue consuming until we find a newline without continuation
+                            self.consume_until(target);
+                            return;
+                        }
+                        _ => break, // Not a continuation
+                    }
+                }
+                break;
+            }
+        }
+    }
+}
