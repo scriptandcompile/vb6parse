86d657254a6908a210aab671891af363c01ecdbf
ScriptAndCompile <scriptandcompile@gmail.com>
2025-11-21 12:54:44 -0800

    Added support for parsing Seek statements

--- src/parsers/syntaxkind.rs
+++ src/parsers/syntaxkind.rs
@@ -59,6 +59,7 @@ pub enum SyntaxKind {
     ResetStatement,
     SavePictureStatement,
     SaveSettingStatement,
+    SeekStatement,
     RandomizeStatement,
     ErrorStatement,
     FileCopyStatement,
