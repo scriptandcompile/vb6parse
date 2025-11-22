8fd354e69bda8fe0dbd23d1480a96c98bd3f0e35
ScriptAndCompile <scriptandcompile@gmail.com>
2025-11-21 18:35:23 -0800

    Added support for parsing DateSerial built in function.

--- src/parsers/cst/library_functions/mod.rs
+++ src/parsers/cst/library_functions/mod.rs
@@ -22,6 +22,7 @@
 //! - DateAdd: Returns a date to which a specified time interval has been added
 //! - DateDiff: Returns the number of time intervals between two dates
 //! - DatePart: Returns a specified part of a given date
+//! - DateSerial: Returns a date for a specified year, month, and day
 //!
 //! Note: Unlike library statements (which are keywords), library functions are
 //! called like regular functions and are parsed as CallExpression nodes in the CST.
@@ -44,3 +45,4 @@ mod date;
 mod dateadd;
 mod datediff;
 mod datepart;
+mod dateserial;
