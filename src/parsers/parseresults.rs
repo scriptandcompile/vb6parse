9c5cbc8d5a47d7df79773820ff84974beb08e7c1
ScriptAndCompile <scriptandcompile@gmail.com>
2025-12-15 18:16:13 -0800

    Added further documentation to ParseResult

--- src/parsers/parseresults.rs
+++ src/parsers/parseresults.rs
@@ -12,9 +12,34 @@ use crate::tokenstream::TokenStream;
 /// * `'a`: Lifetime parameter for error details.
 /// * `T`: The type of the successful parse result.
 /// * `E`: The type of the error details.
+/// 
+/// `ParseResult` is used across the parsing module to encapsulate the outcome of parsing operations,
+/// providing both the parsed data (if any) and any errors or warnings that occurred. `ParseResult` is
+/// used instead of `Result` to allow for partial successes where some data may be parsed correctly
+/// while still reporting errors. This is particularly useful in scenarios where complete failure is not necessary
+/// to halt processing, and where users may want to see all issues in a single pass.
+/// 
+/// # Examples
+/// ```rust
+/// use vb6parse::parsers::parseresults::ParseResult;
+/// use vb6parse::errors::ErrorDetails;
+/// 
+/// let success_result: ParseResult<&str, &str> = ParseResult {
+///     result: Some("Parsed Successfully"),
+///     failures: vec![],
+/// };
+/// assert!(success_result.has_result());
+/// let failure_result: ParseResult<&str, &str> = ParseResult {
+///     result: None,
+///     failures: vec![],
+/// };
+/// assert!(!failure_result.has_result());
+/// ```
 #[derive(Debug, Clone)]
 pub struct ParseResult<'a, T, E> {
+    /// The successful parse result, if any.
     pub result: Option<T>,
+    /// A list of failures encountered during parsing.
     pub failures: Vec<ErrorDetails<'a, E>>,
 }
 
@@ -51,6 +76,34 @@ impl<'a, T, E> ParseResult<'a, T, E> {
     /// # Returns
     /// * `true` if there are one or more failures in the parse result.
     /// * `false` if there are no failures.
+    /// 
+    /// # Examples
+    /// ```rust
+    /// use std::borrow::Cow;
+    /// 
+    /// use vb6parse::parsers::parseresults::ParseResult;
+    /// use vb6parse::errors::{ErrorDetails, CodeErrorKind};
+    /// 
+    /// let failure = ErrorDetails {
+    ///     source_name: "test.bas".to_string(),
+    ///     source_content: Cow::Borrowed("Some source code"),
+    ///     error_offset: 5,
+    ///     line_start: 0,
+    ///     line_end: 10,
+    ///     kind: CodeErrorKind::UnknownToken { token: "???".to_string() },
+    /// };
+    /// let failure_result: ParseResult<'_, &str, CodeErrorKind> = ParseResult {
+    ///     result: None,
+    ///     failures: vec![failure],
+    /// };
+    /// assert!(failure_result.has_failures());
+    /// 
+    /// let success_result: ParseResult<'_, &str, CodeErrorKind> = ParseResult {
+    ///     result: Some("Parsed Successfully"),
+    ///     failures: vec![],
+    /// };
+    /// assert!(!success_result.has_failures());
+    /// ```
     #[inline]
     pub const fn has_failures(&self) -> bool {
         !matches!(self.failures.len(), 0)
@@ -60,6 +113,29 @@ impl<'a, T, E> ParseResult<'a, T, E> {
     ///
     /// # Arguments
     /// * `failure`: An `ErrorDetails` instance representing the failure to be added.
+    /// 
+    /// # Examples
+    /// ```rust
+    /// use std::borrow::Cow;
+    /// 
+    /// use vb6parse::parsers::parseresults::ParseResult;
+    /// use vb6parse::errors::{ErrorDetails, CodeErrorKind};
+    /// 
+    /// let mut parse_result = ParseResult {
+    ///     result: Some("Parsed Successfully"),
+    ///     failures: vec![],
+    /// };
+    /// let failure = ErrorDetails {
+    ///     source_name: "test.bas".to_string(),
+    ///     source_content: Cow::Borrowed("Some source code"),
+    ///     error_offset: 5,
+    ///     line_start: 0,
+    ///     line_end: 10,
+    ///     kind: CodeErrorKind::UnknownToken { token: "???".to_string() },
+    /// };
+    /// parse_result.push_failure(failure);
+    /// assert!(parse_result.has_failures());
+    /// ```
     #[inline]
     pub fn push_failure(&mut self, failure: ErrorDetails<'a, E>) {
         self.failures.push(failure);
@@ -69,6 +145,39 @@ impl<'a, T, E> ParseResult<'a, T, E> {
     ///
     /// # Arguments
     /// * `failures`: A mutable reference to a vector of `ErrorDetails` instances representing the failures to be added.
+    /// 
+    /// # Examples
+    /// ```rust
+    /// use std::borrow::Cow;
+    /// 
+    /// use vb6parse::parsers::parseresults::ParseResult;
+    /// use vb6parse::errors::{ErrorDetails, CodeErrorKind};
+    /// 
+    /// let mut parse_result = ParseResult {
+    ///     result: Some("Parsed Successfully"),
+    ///     failures: vec![],
+    /// };
+    /// let mut failures = vec![
+    ///     ErrorDetails {
+    ///         source_name: "test.bas".to_string(),
+    ///         source_content: Cow::Borrowed("Some source code"),
+    ///         error_offset: 5,
+    ///         line_start: 0,
+    ///         line_end: 10,
+    ///         kind: CodeErrorKind::UnknownToken { token: "???".to_string() },
+    ///     },
+    ///     ErrorDetails {
+    ///         source_name: "test.bas".to_string(),
+    ///         source_content: Cow::Borrowed("Some more source code"),
+    ///         error_offset: 15,
+    ///         line_start: 1,
+    ///         line_end: 11,
+    ///         kind: CodeErrorKind::UnknownToken { token: "???".to_string() },
+    ///     },
+    /// ];
+    /// parse_result.append_failures(&mut failures);
+    /// assert!(parse_result.has_failures());
+    /// ```
     #[inline]
     pub fn append_failures(&mut self, failures: &mut Vec<ErrorDetails<'a, E>>) {
         self.failures.append(failures);
@@ -81,6 +190,18 @@ impl<'a, T, E> ParseResult<'a, T, E> {
     ///
     /// # Returns
     /// * The successful parse result of type `T`.
+    /// 
+    /// # Examples
+    /// ```rust
+    /// use vb6parse::parsers::parseresults::ParseResult;
+    /// 
+    /// let parse_result: ParseResult<&str, &str> = ParseResult {
+    ///     result: Some("Parsed Successfully"),
+    ///     failures: vec![],
+    /// };
+    /// let result = parse_result.unwrap();
+    /// assert_eq!(result, "Parsed Successfully");
+    /// ```
     #[inline]
     pub fn unwrap(self) -> T {
         self.result
@@ -96,6 +217,29 @@ impl<'a, T, E> From<(T, ErrorDetails<'a, E>)> for ParseResult<'a, T, E> {
     ///
     /// # Returns
     /// * A `ParseResult` instance containing the successful result and a vector with the single failure.
+    /// 
+    /// # Examples
+    /// ```rust
+    /// use std::borrow::Cow;
+    /// 
+    /// use vb6parse::parsers::parseresults::ParseResult;
+    /// use vb6parse::errors::{ErrorDetails, CodeErrorKind };
+    /// 
+    /// let failure = ErrorDetails {
+    ///     source_name: "test.bas".to_string(),
+    ///     source_content: Cow::Borrowed("Some source code"),
+    ///     error_offset: 5,
+    ///     line_start: 0,
+    ///     line_end: 10,
+    ///     kind: CodeErrorKind::UnknownToken { token: "???".to_string() },
+    /// };
+    /// 
+    /// let parse_pair = ("Parsed Successfully", failure);
+    /// let parse_result = ParseResult::from(parse_pair);
+    /// 
+    /// assert!(parse_result.has_result());
+    /// assert!(parse_result.has_failures());
+    /// ```
     fn from(parse_pair: (T, ErrorDetails<'a, E>)) -> ParseResult<'a, T, E> {
         ParseResult {
             result: Some(parse_pair.0),
@@ -116,6 +260,39 @@ where
     /// # Returns
     /// * A `ParseResult` instance containing the collected results and the provided failures.
     ///   If the collection is empty, the result will be `None`.
+    /// 
+    /// # Examples
+    /// ```rust
+    /// use std::borrow::Cow;
+    /// 
+    /// use vb6parse::parsers::parseresults::ParseResult;
+    /// use vb6parse::errors::{ErrorDetails, CodeErrorKind};
+    /// 
+    /// let failures = vec![
+    ///     ErrorDetails {
+    ///         source_name: "test.bas".to_string(),
+    ///         source_content: Cow::Borrowed("Some source code"),
+    ///         error_offset: 5,
+    ///         line_start: 0,
+    ///         line_end: 10,
+    ///         kind: CodeErrorKind::UnknownToken { token: "???".to_string() },
+    ///     },
+    ///     ErrorDetails {
+    ///         source_name: "test.bas".to_string(),
+    ///         source_content: Cow::Borrowed("Some source code"),
+    ///         error_offset: 15,
+    ///         line_start: 0,
+    ///         line_end: 10,
+    ///         kind: CodeErrorKind::UnknownToken { token: "???".to_string() },
+    ///     },
+    /// ];
+    /// 
+    /// let parse_pair = (vec!["Item1", "Item2"], failures);
+    /// let parse_result = ParseResult::from(parse_pair);
+    /// 
+    /// assert!(parse_result.has_result());
+    /// assert!(parse_result.has_failures());
+    /// ```
     fn from(parse_pair: (I, Vec<ErrorDetails<'a, E>>)) -> ParseResult<'a, Vec<T>, E> {
         let collection: Vec<T> = parse_pair.0.into_iter().collect();
         if collection.is_empty() {
@@ -142,6 +319,41 @@ impl<'a, E> From<(TokenStream<'a>, Vec<ErrorDetails<'a, E>>)>
     ///
     /// # Returns
     /// * A `ParseResult` instance containing the `TokenStream` and the provided failures.
+    /// 
+    /// # Examples
+    /// ```rust
+    /// use std::borrow::Cow;
+    /// 
+    /// use vb6parse::parsers::parseresults::ParseResult;
+    /// use vb6parse::errors::{ErrorDetails, CodeErrorKind};
+    /// use vb6parse::tokenstream::TokenStream;
+    /// 
+    /// let failures = vec![
+    ///     ErrorDetails {
+    ///         source_name: "test.bas".to_string(),
+    ///         source_content: Cow::Borrowed("Some source code"),
+    ///         error_offset: 5,
+    ///         line_start: 0,
+    ///         line_end: 10,
+    ///         kind: CodeErrorKind::UnknownToken { token: "???".to_string() },
+    ///     },
+    ///     ErrorDetails {
+    ///         source_name: "test.bas".to_string(),
+    ///         source_content: Cow::Borrowed("Some source code"),
+    ///         error_offset: 15,
+    ///         line_start: 0,
+    ///         line_end: 10,
+    ///         kind: CodeErrorKind::UnknownToken { token: "???".to_string() },
+    ///     },
+    /// ];
+    /// 
+    /// let token_stream = TokenStream::new("test.bas".to_string(), vec![]); 
+    /// let parse_pair = (token_stream, failures);
+    /// let parse_result: ParseResult<TokenStream, CodeErrorKind> = ParseResult::from(parse_pair);
+    /// 
+    /// assert!(parse_result.has_result());
+    /// assert!(parse_result.has_failures());
+    /// ```
     fn from(
         parse_pair: (TokenStream<'a>, Vec<ErrorDetails<'a, E>>),
     ) -> ParseResult<'a, TokenStream<'a>, E> {
