bba982c4ae929a41406ff19fd8e9a7e83de4c462
ScriptAndCompile <scriptandcompile@gmail.com>
2025-12-17 12:34:59 -0800

    Doc comments for controls/timer.rs

--- src/language/controls/timer.rs
+++ src/language/controls/timer.rs
@@ -1,3 +1,11 @@
+//! Properties for a `Timer` control.
+//!
+//! This is used as an enum variant of
+//! [`ControlKind::Timer`](crate::language::controls::ControlKind::Timer).
+//! tag, name, and index are not included in this struct, but instead are part
+//! of the parent [`Control`](crate::language::controls::Control) struct.
+//!
+
 use crate::language::controls::Activation;
 
 use crate::parsers::Properties;
@@ -10,9 +18,13 @@ use crate::parsers::Properties;
 /// of the parent [`Control`](crate::language::controls::Control) struct.
 #[derive(Debug, PartialEq, Eq, Clone, serde::Serialize)]
 pub struct TimerProperties {
+    /// Whether the timer is enabled.
     pub enabled: Activation,
+    /// The timer interval in milliseconds.
     pub interval: i32,
+    /// The left position of the timer control.
     pub left: i32,
+    /// The top position of the timer control.
     pub top: i32,
 }
 
