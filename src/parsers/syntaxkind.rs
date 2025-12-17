61f1d004a5d93859e22711dcd8b36d298831bbaa
ScriptAndCompile <scriptandcompile@gmail.com>
2025-12-17 14:05:08 -0800

    Doc comments for parsers/syntaxkind.rs

--- src/parsers/syntaxkind.rs
+++ src/parsers/syntaxkind.rs
@@ -1,3 +1,15 @@
+//! Syntax kinds for the VB6 CST.
+//!
+//! This module defines the `SyntaxKind` enum, which represents all possible
+//! node and token types in the Concrete Syntax Tree (CST) for VB6 code.
+//!
+//! Each variant of the enum corresponds to a specific syntactic construct
+//! in the VB6 language, including statements, expressions, keywords, literals,
+//! operators, and punctuation.
+//!
+//! The `SyntaxKind` enum is used throughout the parser to identify and
+//! categorize different parts of the syntax tree.
+
 use crate::language::Token;
 
 /// Syntax kinds for the VB6 CST.
@@ -6,332 +18,631 @@ use crate::language::Token;
 #[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord, Hash, serde::Serialize)]
 #[repr(u16)]
 pub enum SyntaxKind {
-    // Root node
+    /// Root node of the syntax tree
     Root = 0,
 
     // Statement nodes
+    /// Module definition statement
     ModuleStatement,
+    /// Class definition statement
     ClassStatement,
+    /// Sub procedure statement
     SubStatement,
+    /// Function procedure statement
     FunctionStatement,
+    /// Property procedure statement
     PropertyStatement,
+    /// Declare statement
     DeclareStatement,
+    /// Event declaration statement
     EventStatement,
+    /// Implements statement
     ImplementsStatement,
+    /// DefType statement
     DefTypeStatement,
+    /// Dim statement
     DimStatement,
+    /// ReDim statement
     ReDimStatement,
+    /// Erase statement
     EraseStatement,
+    /// Const statement
     ConstStatement,
+    /// Type statement
     TypeStatement,
+    /// Enum statement
     EnumStatement,
+    /// If statement
     IfStatement,
+    /// ElseIf clause of an If statement
     ElseIfClause,
+    /// Else clause of an If statement
     ElseClause,
+    /// For statement
     ForStatement,
+    /// For Each statement
     ForEachStatement,
+    /// While statement
     WhileStatement,
+    /// Do statement
     DoStatement,
+    /// Select Case statement
     SelectCaseStatement,
+    /// Case clause of a Select Case statement
     CaseClause,
+    /// Case Else clause of a Select Case statement
     CaseElseClause,
+    /// With statement
     WithStatement,
+    /// Call statement
     CallStatement,
+    /// RaiseEvent statement
     RaiseEventStatement,
+    /// Set statement
     SetStatement,
+    /// Let statement
     LetStatement,
+    /// Assignment statement
     AssignmentStatement,
+    /// Goto statement
     GotoStatement,
+    /// GoSub statement
     GoSubStatement,
+    /// Return statement
     ReturnStatement,
+    /// Resume statement
     ResumeStatement,
+    /// Exit statement
     ExitStatement,
+    /// On Error statement
     OnErrorStatement,
+    /// On GoTo statement
     OnGoToStatement,
+    /// On GoSub statement
     OnGoSubStatement,
+    /// AppActivate statement
     AppActivateStatement,
+    /// Beep statement
     BeepStatement,
+    /// ChDir statement
     ChDirStatement,
+    /// ChDrive statement
     ChDriveStatement,
+    /// Close statement
     CloseStatement,
+    /// Date statement
     DateStatement,
+    /// DeleteSetting statement
     DeleteSettingStatement,
+    /// Reset statement
     ResetStatement,
+    /// SavePicture statement
     SavePictureStatement,
+    /// SaveSetting statement
     SaveSettingStatement,
+    /// Seek statement
     SeekStatement,
+    /// SendKeys statement
     SendKeysStatement,
+    /// SetAttr statement
     SetAttrStatement,
+    /// Stop statement
     StopStatement,
+    /// Time statement
     TimeStatement,
+    /// Randomize statement
     RandomizeStatement,
+    /// Error statement
     ErrorStatement,
+    /// FileCopy statement
     FileCopyStatement,
+    /// Get statement
     GetStatement,
+    /// Put statement
     PutStatement,
+    /// Input statement
     InputStatement,
+    /// LineInput statement
     LineInputStatement,
+    /// Kill statement
     KillStatement,
+    /// Load statement
     LoadStatement,
+    /// Unload statement
     UnloadStatement,
+    /// Lock statement
     LockStatement,
+    /// Unlock statement
     UnlockStatement,
+    /// LSet statement
     LSetStatement,
+    /// RSet statement
     RSetStatement,
+    /// Mid statement
     MidStatement,
+    /// MidB statement
     MidBStatement,
+    /// MkDir statement
     MkDirStatement,
+    /// RmDir statement
     RmDirStatement,
+    /// Name statement
     NameStatement,
+    /// Open statement
     OpenStatement,
+    /// Print statement
     PrintStatement,
+    /// Width statement
     WidthStatement,
+    /// Write statement
     WriteStatement,
+    /// Label statement
     LabelStatement,
+    /// Attribute statement
     AttributeStatement,
+    /// Option statement
     OptionStatement,
+    /// Object statement
     ObjectStatement,
 
     // Class/Form Header nodes
+    /// Version statement
     VersionStatement,
+    /// Properties block
     PropertiesBlock,
+    /// Properties type declaration
     PropertiesType,
+    /// Properties name declaration
     PropertiesName,
+    /// Property Statement
     Property,
+    /// Property key
     PropertyKey,
+    /// Property value
     PropertyValue,
+    /// Property group
     PropertyGroup,
+    /// Property group name
     PropertyGroupName,
 
     // Expression nodes
+    /// Binary expression
     BinaryExpression,
+    /// Unary expression
     UnaryExpression,
+    /// Literal expression
     LiteralExpression,
+    /// Identifier expression
     IdentifierExpression,
+    /// Member access expression
     MemberAccessExpression,
+    /// Call expression
     CallExpression,
+    /// Parenthesized expression
     ParenthesizedExpression,
+    /// Numeric literal expression
     NumericLiteralExpression,
+    /// String literal expression
     StringLiteralExpression,
+    /// Boolean literal expression
     BooleanLiteralExpression,
+    /// AddressOf expression
     AddressOfExpression,
+    /// TypeOf expression
     TypeOfExpression,
+    /// New expression
     NewExpression,
 
-    // Conditional nodes
-    BinaryConditional,
-    UnaryConditional,
-
     // Other structural nodes
+    /// Argument list
     ArgumentList,
+    /// Parameter list
     ParameterList,
+    /// Parameter
     Parameter,
+    /// Argument
     Argument,
+    /// Block of code/statements
     CodeBlock,
 
-    // Token kinds - map from VB6Token
+    // Token kinds - map from Token
     // We start these at a higher offset to avoid conflicts
+    /// Whitespace token
     Whitespace = 1000,
+    /// Newline token
     Newline,
+    /// End-of-line comment token
     EndOfLineComment,
+    /// Rem comment token
     RemComment,
 
     // Keywords
+    /// Class keyword
     ClassKeyword,
+    /// ReDim keyword
     ReDimKeyword,
+    /// Preserve keyword
     PreserveKeyword,
+    /// Dim keyword
     DimKeyword,
+    /// Declare keyword
     DeclareKeyword,
+    /// Alias keyword
     AliasKeyword,
+    /// Attribute keyword
     AttributeKeyword,
+    /// Begin keyword
     BeginKeyword,
+    /// Lib keyword
     LibKeyword,
+    /// With keyword
     WithKeyword,
+    /// WithEvents keyword
     WithEventsKeyword,
+    /// Base keyword
     BaseKeyword,
+    /// Compare keyword
     CompareKeyword,
+    /// Option keyword
     OptionKeyword,
+    /// Explicit keyword
     ExplicitKeyword,
+    /// Private keyword
     PrivateKeyword,
+    /// Public keyword
     PublicKeyword,
+    /// Const keyword
     ConstKeyword,
+    /// As keyword
     AsKeyword,
+    /// ByVal keyword
     ByValKeyword,
+    /// ByRef keyword
     ByRefKeyword,
+    /// Optional keyword
     OptionalKeyword,
+    /// Function keyword
     FunctionKeyword,
+    /// Static keyword
     StaticKeyword,
+    /// Sub keyword
     SubKeyword,
+    /// End keyword
     EndKeyword,
+    /// True keyword
     TrueKeyword,
+    /// False keyword
     FalseKeyword,
+    /// Enum keyword
     EnumKeyword,
+    /// Type keyword
     TypeKeyword,
+    /// Boolean keyword
     BooleanKeyword,
+    /// Double keyword
     DoubleKeyword,
+    /// Currency keyword
     CurrencyKeyword,
+    /// Decimal keyword
     DecimalKeyword,
+    /// Date keyword
     DateKeyword,
+    /// Object keyword
     ObjectKeyword,
+    /// Variant keyword
     VariantKeyword,
+    /// Byte keyword
     ByteKeyword,
+    /// Long keyword
     LongKeyword,
+    /// Single keyword
     SingleKeyword,
+    /// String keyword
     StringKeyword,
+    /// Integer keyword
     IntegerKeyword,
+    /// If keyword
     IfKeyword,
+    /// Else keyword
     ElseKeyword,
+    /// ElseIf keyword
     ElseIfKeyword,
+    /// And keyword
     AndKeyword,
+    /// Or keyword
     OrKeyword,
+    /// Xor keyword
     XorKeyword,
+    /// Mod keyword
     ModKeyword,
+    /// Eqv keyword
     EqvKeyword,
+    /// AddressOf keyword
     AddressOfKeyword,
+    /// Imp keyword
     ImpKeyword,
+    /// Is keyword
     IsKeyword,
+    /// Like keyword
     LikeKeyword,
+    /// Not keyword
     NotKeyword,
+    /// Then keyword
     ThenKeyword,
+    /// Goto keyword
     GotoKeyword,
+    /// GoSub keyword
     GoSubKeyword,
+    /// Return keyword
     ReturnKeyword,
+    /// Exit keyword
     ExitKeyword,
+    /// For keyword
     ForKeyword,
+    /// Each keyword
     EachKeyword,
+    /// In keyword
     InKeyword,
+    /// To keyword
     ToKeyword,
+    /// Lock keyword
     LockKeyword,
+    /// Unlock keyword
     UnlockKeyword,
+    /// Step keyword
     StepKeyword,
+    /// Stop keyword
     StopKeyword,
+    /// While keyword
     WhileKeyword,
+    /// Wend keyword
     WendKeyword,
+    /// Width keyword
     WidthKeyword,
+    /// Write keyword
     WriteKeyword,
+    /// Time keyword
     TimeKeyword,
+    /// SetAttr keyword
     SetAttrKeyword,
+    /// Set keyword
     SetKeyword,
+    /// SendKeys keyword
     SendKeysKeyword,
+    /// Select keyword
     SelectKeyword,
+    /// Case keyword
     CaseKeyword,
+    /// Seek keyword
     SeekKeyword,
+    /// SaveSetting keyword
     SaveSettingKeyword,
+    /// SavePicture keyword
     SavePictureKeyword,
+    /// RSet keyword
     RSetKeyword,
+    /// RmDir keyword
     RmDirKeyword,
+    /// Resume keyword
     ResumeKeyword,
+    /// Reset keyword
     ResetKeyword,
+    /// Randomize keyword
     RandomizeKeyword,
+    /// RaiseEvent keyword
     RaiseEventKeyword,
+    /// Put keyword
     PutKeyword,
+    /// Property keyword
     PropertyKeyword,
+    /// Print keyword
     PrintKeyword,
+    /// Open keyword
     OpenKeyword,
+    /// On keyword
     OnKeyword,
+    /// Off keyword
     OffKeyword,
+    /// Name keyword
     NameKeyword,
+    /// MkDir keyword
     MkDirKeyword,
+    /// Mid keyword
     MidKeyword,
+    /// MidB keyword
     MidBKeyword,
+    /// LSet keyword
     LSetKeyword,
+    /// Load keyword
     LoadKeyword,
+    /// Unload keyword
     UnloadKeyword,
+    /// Line keyword
     LineKeyword,
+    /// Input keyword
     InputKeyword,
+    /// Let keyword
     LetKeyword,
+    /// Kill keyword
     KillKeyword,
+    /// Implies keyword
     ImplementsKeyword,
+    /// Get keyword
     GetKeyword,
+    /// FileCopy keyword
     FileCopyKeyword,
+    /// Event keyword
     EventKeyword,
+    /// Error keyword
     ErrorKeyword,
+    /// Erase keyword
     EraseKeyword,
+    /// Do keyword
     DoKeyword,
+    /// Until keyword
     UntilKeyword,
+    /// DeleteSetting keyword
     DeleteSettingKeyword,
+    /// DefBool keyword
     DefBoolKeyword,
+    /// DefByte keyword
     DefByteKeyword,
+    /// DefInt keyword
     DefIntKeyword,
+    /// DefLng keyword
     DefLngKeyword,
+    /// DefCur keyword
     DefCurKeyword,
+    /// DefSng keyword
     DefSngKeyword,
+    /// DefDbl keyword
     DefDblKeyword,
+    /// DefDec keyword
     DefDecKeyword,
+    /// DefDate keyword
     DefDateKeyword,
+    /// DefStr keyword
     DefStrKeyword,
+    /// DefObj keyword
     DefObjKeyword,
+    /// DefVar keyword
     DefVarKeyword,
+    /// Close keyword
     CloseKeyword,
+    /// ChDrive keyword
     ChDriveKeyword,
+    /// ChDir keyword
     ChDirKeyword,
+    /// Call keyword
     CallKeyword,
+    /// Beep keyword
     BeepKeyword,
+    /// AppActivate keyword
     AppActivateKeyword,
+    /// Friend keyword
     FriendKeyword,
+    /// Binary keyword
     BinaryKeyword,
+    /// Random keyword
     RandomKeyword,
+    /// Read keyword
     ReadKeyword,
+    /// Output keyword
     OutputKeyword,
+    /// Append keyword
     AppendKeyword,
+    /// Access keyword
     AccessKeyword,
+    /// Text keyword
     TextKeyword,
+    /// Database keyword
     DatabaseKeyword,
+    /// Empty keyword
     EmptyKeyword,
+    /// Module keyword
     ModuleKeyword,
+    /// Next keyword
     NextKeyword,
+    /// New keyword
     NewKeyword,
+    /// Len keyword
     LenKeyword,
+    /// Me keyword
     MeKeyword,
+    /// Null keyword
     NullKeyword,
+    /// ParamArray keyword
     ParamArrayKeyword,
+    /// Version keyword
     VersionKeyword,
+    /// Loop keyword
     LoopKeyword,
+    /// Nothing keyword
     NothingKeyword,
+    /// Any keyword
     AnyKeyword,
 
     // Literals and identifiers
+    /// Identifier token
     Identifier,
+    /// String literal token
     StringLiteral,
+    /// Integer literal token
     IntegerLiteral,
+    /// Long literal token
     LongLiteral,
+    /// Single literal token
     SingleLiteral,
+    /// Double literal token
     DoubleLiteral,
+    /// Decimal literal token
     DecimalLiteral,
+    /// Currency literal token
     CurrencyLiteral,
+    /// Date literal token
     DateLiteral,
 
     // Operators and punctuation
+    /// Dollar sign token
     DollarSign,
+    /// Underscore token
     Underscore,
+    /// Ampersand token
     Ampersand,
+    /// Percent token
     Percent,
+    /// Octothorpe '#' token
     Octothorpe,
+    /// Left parenthesis '(' token
     LeftParenthesis,
+    /// Right parenthesis ')' token
     RightParenthesis,
+    /// Left square bracket '[' token
     LeftSquareBracket,
+    /// Right square bracket ']' token
     RightSquareBracket,
+    /// Comma ',' token
     Comma,
+    /// Semicolon ';' token
     Semicolon,
+    /// At sign '@' token
     AtSign,
+    /// Exclamation mark '!' token
     ExclamationMark,
+    /// Equality operator '=' token
     EqualityOperator,
+    /// Inequality operator '<>' token
     InequalityOperator,
+    /// Less than or equal operator '<=' token
     LessThanOrEqualOperator,
+    /// Greater than or equal operator '>=' token
     GreaterThanOrEqualOperator,
+    /// Less than operator '<' token
     LessThanOperator,
+    /// Greater than operator '>' token
     GreaterThanOperator,
+    /// Multiplication operator '*' token
     MultiplicationOperator,
+    /// Subtraction operator '-' token
     SubtractionOperator,
+    /// Addition operator '+' token
     AdditionOperator,
+    /// Division operator '/' token
     DivisionOperator,
+    /// Backward slash operator '\' token
     BackwardSlashOperator,
+    /// Period operator '.' token
     PeriodOperator,
+    /// Colon operator ':' token
     ColonOperator,
+    /// Exponentiation operator '^' token
     ExponentiationOperator,
 
     // Error recovery
+    /// Error node for error recovery
     Error,
+
+    /// Unknown syntax kind
     Unknown,
 }
 
