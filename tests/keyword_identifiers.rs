9b6b47cb6776b4ea7c685e548cf3bcc0fb92035e
ScriptAndCompile <scriptandcompile@gmail.com>
2026-01-07 16:30:11 -0800

    Integration insta tests for tests/keyword_identifiers now have their snapshots moved to /snapshots/tests/keyword_identifiers

--- tests/keyword_identifiers.rs
+++ tests/keyword_identifiers.rs
@@ -11,24 +11,13 @@ fn keyword_as_sub_name() {
     let source = "Sub Text()\nEnd Sub\n";
     let (cst_opt, _failures) = ConcreteSyntaxTree::from_text("test.bas", source).unpack();
     let cst = cst_opt.expect("CST should be parsed");
+    let tree = cst.to_serializable();
 
-    assert_tree!(cst, [
-        SubStatement {
-            SubKeyword,
-            Whitespace,
-            Identifier ("Text"),
-            ParameterList {
-                LeftParenthesis,
-                RightParenthesis,
-            },
-            Newline,
-            StatementList,
-            EndKeyword,
-            Whitespace,
-            SubKeyword,
-            Newline,
-        },
-    ]);
+    let mut settings = insta::Settings::clone_current();
+    settings.set_snapshot_path("../snapshots/tests/keyword_identifiers");
+    settings.set_prepend_module_to_snapshot(false);
+    let _guard = settings.bind_to_scope();
+    insta::assert_yaml_snapshot!(tree);
 }
 
 #[test]
@@ -36,28 +25,13 @@ fn keyword_as_function_name() {
     let source = "Function Database() As String\nEnd Function\n";
     let (cst_opt, _failures) = ConcreteSyntaxTree::from_text("test.bas", source).unpack();
     let cst = cst_opt.expect("CST should be parsed");
+    let tree = cst.to_serializable();
 
-    assert_tree!(cst, [
-        FunctionStatement {
-            FunctionKeyword,
-            Whitespace,
-            Identifier ("Database"),
-            ParameterList {
-                LeftParenthesis,
-                RightParenthesis,
-            },
-            Whitespace,
-            AsKeyword,
-            Whitespace,
-            StringKeyword,
-            Newline,
-            StatementList,
-            EndKeyword,
-            Whitespace,
-            FunctionKeyword,
-            Newline,
-        },
-    ]);
+    let mut settings = insta::Settings::clone_current();
+    settings.set_snapshot_path("../snapshots/tests/keyword_identifiers");
+    settings.set_prepend_module_to_snapshot(false);
+    let _guard = settings.bind_to_scope();
+    insta::assert_yaml_snapshot!(tree);
 }
 
 #[test]
@@ -65,30 +39,13 @@ fn keyword_as_property_name() {
     let source = "Property Get Binary() As Integer\nEnd Property\n";
     let (cst_opt, _failures) = ConcreteSyntaxTree::from_text("test.bas", source).unpack();
     let cst = cst_opt.expect("CST should be parsed");
+    let tree = cst.to_serializable();
 
-    assert_tree!(cst, [
-        PropertyStatement {
-            PropertyKeyword,
-            Whitespace,
-            GetKeyword,
-            Whitespace,
-            Identifier ("Binary"),
-            ParameterList {
-                LeftParenthesis,
-                RightParenthesis,
-            },
-            Whitespace,
-            AsKeyword,
-            Whitespace,
-            IntegerKeyword,
-            Newline,
-            StatementList,
-            EndKeyword,
-            Whitespace,
-            PropertyKeyword,
-            Newline,
-        },
-    ]);
+    let mut settings = insta::Settings::clone_current();
+    settings.set_snapshot_path("../snapshots/tests/keyword_identifiers");
+    settings.set_prepend_module_to_snapshot(false);
+    let _guard = settings.bind_to_scope();
+    insta::assert_yaml_snapshot!(tree);
 }
 
 #[test]
@@ -96,21 +53,13 @@ fn keyword_as_variable_in_assignment() {
     let source = "text = \"hello\"\n";
     let (cst_opt, _failures) = ConcreteSyntaxTree::from_text("test.bas", source).unpack();
     let cst = cst_opt.expect("CST should be parsed");
+    let tree = cst.to_serializable();
 
-    assert_tree!(cst, [
-        AssignmentStatement {
-            IdentifierExpression {
-                TextKeyword,
-            },
-            Whitespace,
-            EqualityOperator,
-            Whitespace,
-            StringLiteralExpression {
-                StringLiteral ("\"hello\""),
-            },
-            Newline,
-        },
-    ]);
+    let mut settings = insta::Settings::clone_current();
+    settings.set_snapshot_path("../snapshots/tests/keyword_identifiers");
+    settings.set_prepend_module_to_snapshot(false);
+    let _guard = settings.bind_to_scope();
+    insta::assert_yaml_snapshot!(tree);
 }
 
 #[test]
@@ -118,23 +67,13 @@ fn keyword_as_property_in_assignment() {
     let source = "obj.text = \"hello\"\n";
     let (cst_opt, _failures) = ConcreteSyntaxTree::from_text("test.bas", source).unpack();
     let cst = cst_opt.expect("CST should be parsed");
+    let tree = cst.to_serializable();
 
-    assert_tree!(cst, [
-        AssignmentStatement {
-            MemberAccessExpression {
-                Identifier ("obj"),
-                PeriodOperator,
-                TextKeyword,
-            },
-            Whitespace,
-            EqualityOperator,
-            Whitespace,
-            StringLiteralExpression {
-                StringLiteral ("\"hello\""),
-            },
-            Newline,
-        },
-    ]);
+    let mut settings = insta::Settings::clone_current();
+    settings.set_snapshot_path("../snapshots/tests/keyword_identifiers");
+    settings.set_prepend_module_to_snapshot(false);
+    let _guard = settings.bind_to_scope();
+    insta::assert_yaml_snapshot!(tree);
 }
 
 #[test]
@@ -146,48 +85,13 @@ obj.binary = True
 "#;
     let (cst_opt, _failures) = ConcreteSyntaxTree::from_text("test.bas", source).unpack();
     let cst = cst_opt.expect("CST should be parsed");
+    let tree = cst.to_serializable();
 
-    assert_tree!(cst, [
-        Newline,
-        AssignmentStatement {
-            IdentifierExpression {
-                DatabaseKeyword,
-            },
-            Whitespace,
-            EqualityOperator,
-            Whitespace,
-            StringLiteralExpression {
-                StringLiteral ("\"mydb.mdb\""),
-            },
-            Newline,
-        },
-        AssignmentStatement {
-            IdentifierExpression {
-                TextKeyword,
-            },
-            Whitespace,
-            EqualityOperator,
-            Whitespace,
-            StringLiteralExpression {
-                StringLiteral ("\"hello\""),
-            },
-            Newline,
-        },
-        AssignmentStatement {
-            MemberAccessExpression {
-                Identifier ("obj"),
-                PeriodOperator,
-                BinaryKeyword,
-            },
-            Whitespace,
-            EqualityOperator,
-            Whitespace,
-            BooleanLiteralExpression {
-                TrueKeyword,
-            },
-            Newline,
-        },
-    ]);
+    let mut settings = insta::Settings::clone_current();
+    settings.set_snapshot_path("../snapshots/tests/keyword_identifiers");
+    settings.set_prepend_module_to_snapshot(false);
+    let _guard = settings.bind_to_scope();
+    insta::assert_yaml_snapshot!(tree);
 }
 
 #[test]
@@ -195,25 +99,13 @@ fn keyword_as_enum_name() {
     let source = "Enum Random\n    Value1\n    Value2\nEnd Enum\n";
     let (cst_opt, _failures) = ConcreteSyntaxTree::from_text("test.bas", source).unpack();
     let cst = cst_opt.expect("CST should be parsed");
+    let tree = cst.to_serializable();
 
-    assert_tree!(cst, [
-        EnumStatement {
-            EnumKeyword,
-            Whitespace,
-            Identifier ("Random"),
-            Newline,
-            Whitespace,
-            Identifier ("Value1"),
-            Newline,
-            Whitespace,
-            Identifier ("Value2"),
-            Newline,
-            EndKeyword,
-            Whitespace,
-            EnumKeyword,
-            Newline,
-        },
-    ]);
+    let mut settings = insta::Settings::clone_current();
+    settings.set_snapshot_path("../snapshots/tests/keyword_identifiers");
+    settings.set_prepend_module_to_snapshot(false);
+    let _guard = settings.bind_to_scope();
+    insta::assert_yaml_snapshot!(tree);
 }
 
 #[test]
@@ -222,22 +114,11 @@ fn keyword_after_keyword_converted() {
     let source = "Sub Output()\nEnd Sub\n";
     let (cst_opt, _failures) = ConcreteSyntaxTree::from_text("test.bas", source).unpack();
     let cst = cst_opt.expect("CST should be parsed");
+    let tree = cst.to_serializable();
 
-    assert_tree!(cst, [
-        SubStatement {
-            SubKeyword,
-            Whitespace,
-            Identifier ("Output"),
-            ParameterList {
-                LeftParenthesis,
-                RightParenthesis,
-            },
-            Newline,
-            StatementList,
-            EndKeyword,
-            Whitespace,
-            SubKeyword,
-            Newline,
-        },
-    ]);
+    let mut settings = insta::Settings::clone_current();
+    settings.set_snapshot_path("../snapshots/tests/keyword_identifiers");
+    settings.set_prepend_module_to_snapshot(false);
+    let _guard = settings.bind_to_scope();
+    insta::assert_yaml_snapshot!(tree);
 }
